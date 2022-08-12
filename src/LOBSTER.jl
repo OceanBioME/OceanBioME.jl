1ba6f1d3a12f2306a3c3f37b1c58f05ecc8ed5c7
Si Chen <chensi.cs001@gmail.com>
2022-08-12 11:21:38 +0100

    add comments and explanations in LOBSTER.jl

--- src/LOBSTER.jl
+++ src/LOBSTER.jl
@@ -1,18 +1,28 @@
 module LOBSTER
-#https://agupubs.onlinelibrary.wiley.com/doi/epdf/10.1029/2004JC002588
+#=
+The Lodyc Ocean Biogeochemical SimulationTools for Ecosystem and Resources (LOBSTER) model consists of six prognostic variables 
+expressed in terms of their nitrogen content: 
+nitrate (NO₃), ammonium (NH₄), phytoplankton (P), zooplankton (Z), detritus(D) and semilabile dissolved organic matter (DOM).
+Current version has been expanded to include Large detritus (DD) and optional tracers: dissolved inorganic carbon (DIC), alkalinity (ALK) and oxygen (OXY). 
+Details can be found in references: 
+(1)2005 A four-dimensional mesoscale map of the spring bloom in the northeast Atlantic (POMME experiment): Results of a prognostic model
+(2)2001 Impact of sub-mesoscale physics on production and subduction of phytoplankton in an oligotrophic regime
+(3)2012 How does dynamical spatial variability impact 234Th-derived estimates of organic export
+(4)2001 Bio-optical properties of oceanic waters: A reappraisal
+=#
 using Oceananigans
 include("parameters/lobster.jl")
 
-@inline p(P, D, params) = params.p̃*P/(params.p̃*P+(1-params.p̃)*D)
-@inline G_d(P, Z, D, params) = params.g_z*(1-p(P, D, params))*D*Z/(params.K_z+P*p(P, D, params)+(1-p(P, D, params))*D)
-@inline G_p(P, Z, D, params) = params.g_z*p(P, D, params)*P*Z/(params.K_z+P*p(P, D, params)+(1-p(P, D, params))*D)
+@inline p(P, D, params) = params.p̃*P/(params.p̃*P+(1-params.p̃)*D) #Preference for phytoplankton
+@inline G_d(P, Z, D, params) = params.g_z*(1-p(P, D, params))*D*Z/(params.K_z+P*p(P, D, params)+(1-p(P, D, params))*D) #Grazing of detritus
+@inline G_p(P, Z, D, params) = params.g_z*p(P, D, params)*P*Z/(params.K_z+P*p(P, D, params)+(1-p(P, D, params))*D) #Grazing of phytoplankton
 
 #Limiting equations
-@inline L_I(PAR, params) = 1-exp(-PAR/params.K_par) # reference Levy 2001
-@inline L_NO₃(NO₃, NH₄, params) = NO₃*exp(-params.ψ*NH₄)/(NO₃+params.K_no₃)
-@inline L_NH₄(NH₄, params) = max(NH₄/(NH₄+params.K_nh₄),0)    # NH₄/(NH₄+K_nh₄)
+@inline L_I(PAR, params) = 1-exp(-PAR/params.K_par) # reference (4) #Light limitation
+@inline L_NO₃(NO₃, NH₄, params) = NO₃*exp(-params.ψ*NH₄)/(NO₃+params.K_no₃) #Nitrate limitation
+@inline L_NH₄(NH₄, params) = max(NH₄/(NH₄+params.K_nh₄),0)    # NH₄/(NH₄+K_nh₄) #Ammonium limitation
 
-#source functions
+#source functions, refer to Table A1 in reference (1)
 Z_forcing(x, y, z, t, NO₃, NH₄, P, Z, D, DD, DOM, PAR::AbstractFloat, params) = params.a_z*(G_d(P, Z, D, params) + G_p(P, Z, D, params)) - params.m_z*Z^2 - params.μ_z*Z
 D_forcing(x, y, z, t, NO₃, NH₄, P, Z, D, DD, DOM, PAR::AbstractFloat, params) = (1-params.f_d)*(1-params.a_z)*(G_d(P, Z, D, params)+G_p(P, Z, D, params)) + (1-params.f_d)*params.m_p*P^2 - G_d(P, Z, D, params) + params.f_z*params.m_z*Z^2 - params.μ_d*D #- aggreg_D2DD(z, D, DD) + aggreg_DOM2D(z, D, DOM) 
 DD_forcing(x, y, z, t, NO₃, NH₄, P, Z, D, DD, DOM, PAR::AbstractFloat, params) = params.f_d*(1-params.a_z)*(G_d(P, Z, D, params)+G_p(P, Z, D, params)) + params.f_d*params.m_p*P^2 + (1-params.f_z)*params.m_z*Z^2 - params.μ_dd*DD #+ aggreg_D2DD(z, D, DD) + aggreg_DOM2DD(z, DD, DOM) 
@@ -26,8 +36,8 @@ ALK_forcing(x, y, z, t, NO₃, NH₄, P, Z, D, DD, DOM, DIC, ALK, PAR::AbstractF
 
 OXY_forcing(x, y, z, t, NO₃, NH₄, P, Z, D, DD, DOM, OXY, PAR::AbstractFloat, params) = nothing
 
-getPAR(PAR::Field, x, y, z, t) = Oceananigans.Fields.interpolate(PAR, Center(), Center(), Center(), PAR.grid, x, y, z)
-getPAR(PAR_func, x, y, z, t) = PAR_func(x, y, z, t)
+getPAR(PAR::Field, x, y, z, t) = Oceananigans.Fields.interpolate(PAR, Center(), Center(), Center(), PAR.grid, x, y, z) # One can calculate PAR as a field, or use PAR as a function as follows.
+getPAR(PAR_func, x, y, z, t) = PAR_func(x, y, z, t) # use PAR as a function
 
 #source functions with PAR func
 Z_forcing(x, y, z, t, NO₃, NH₄, P, Z, D, DD, DOM, params) = Z_forcing(x, y, z, t, NO₃, NH₄, P, Z, D, DD, DOM, getPAR(params.PAR, x, y, z, t), params)
@@ -43,7 +53,7 @@ ALK_forcing(x, y, z, t, NO₃, NH₄, P, Z, D, DD, DOM, DIC, ALK, params) = ALK_
 
 OXY_forcing(x, y, z, t, NO₃, NH₄, P, Z, D, DD, DOM, OXY, params) = nothing
 
-#advective forcings
+#detritus sinking speed profile 
 D_sinking(z, params) = params.V_d*tanh(max(-z/params.λ,0))
 DD_sinking(z, params) = params.V_dd*tanh(max(-z/params.λ,0))
 
