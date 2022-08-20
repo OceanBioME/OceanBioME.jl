b150a096af3ed2fa187fec1c8d67e1b9c9a3fc5a
Si Chen <sc2350@charybdis.damtp.cam.ac.uk>
2022-08-20 11:32:22 +0100

    add more explanations and comments

--- examples/lobster_example.jl
+++ examples/lobster_example.jl
@@ -1,8 +1,10 @@
 """
-This script illustrates how to run OceanBioME as a 1D column model using the LOBSTER biogeochemical model and idealized mixed layer depth and surface PAR timeseries
-In this case, the script uses an idealized mixed layer depth to construct an idealized diffusivity profile. 
-The script uses a timeseries of the photosynthetically available radiation for forcing the LOBSTER model.
-This script also shows how to interpolate PAR timeseries data to access to arbitrary time. The PAR profile, as an auxiliary field, will be updated every 1 timestep.
+This script illustrates how to run OceanBioME as a 1D column model using the LOBSTER biogeochemical model.
+In this case, the script 
+*uses an idealized mixed layer depth (mld) to construct an idealized diffusivity profile;
+*uses an idealized surface photosynthetically available radiation (PAR) timeseries to derive the PAR profile for forcing the LOBSTER model, according to a light absorption model.
+    The PAR profile will be updated every 1 timestep as an auxiliary field; and
+*shows how to interpolate a timeseries of data to access to arbitrary time. 
 References
 (1) Lévy, M., Gavart, M., Mémery, L., Caniaux, G. and Paci, A., 2005. A four‐dimensional mesoscale map of the spring bloom in the northeast Atlantic (POMME experiment): Results of a prognostic model. Journal of Geophysical Research: Oceans, 110(C7).
 (2) Lévy, M., Klein, P. and Treguier, A.M., 2001. Impact of sub-mesoscale physics on production and subduction of phytoplankton in an oligotrophic regime. Journal of marine research, 59(4), pp.535-565.
@@ -11,7 +13,7 @@ References
 (5) Resplandy, L., Lévy, M., d'Ovidio, F. and Merlivat, L., 2009. Impact of submesoscale variability in estimating the air‐sea CO2 exchange: Results from a model study of the POMME experiment. Global Biogeochemical Cycles, 23(1).
 """
 
-# First, load the needed modules
+# Load the needed modules
 using Random   
 using Printf
 using Plots
@@ -26,22 +28,27 @@ using Oceananigans.Units: second, minute, minutes, hour, hours, day, days, year,
 
 using OceanBioME 
 
-params = LOBSTER.defaults  #load parameters in src/parameters/lobster.jl 
+# Load parameters from src/Models/Biogeochemistry/LOBSTER.jl
+params = LOBSTER.defaults  
 
-# Step 1: import data
-#The mixed layer depth is used to construct an idealized diffusivity profile
-time_mld = [0.,50,86,105,280,364] # in days, specify piecewiselinear time nodes, must be float 
-mld = [250,420,420,20,20,250] # mixed layer depth in meters 
-mld_itp = LinearInterpolation((time_mld)days, mld)  #
+# Create an idealized mixed layer depth data, and interpolate it to access to mixed layer depth at arbitrary time
+time_mld = [0.,50,86,105,280,364] # in days, piecewiselinear time nodes, must be float 
+mld = [250,420,420,20,20,250] # in meters, mixed layer depth 
 
-#The surface PAR timeseries is used to calculate PAR profile
-time_par = [0.,20,120,200,330,364]
-par = [5,5,90,90,5,5]
-PAR_itp = LinearInterpolation((time_par)days, par)
-surface_PAR(t) = PAR_itp(mod(t, 364days))  # will be used as part of Option 2 of PAR_field
+# Convert unit from days to seconds, then conduct linear interpolation to access to mixed layer depth at arbitrary time
+#'days' is a Float64 constant equal to 24hours (defined in Oceananigans). Useful for increasing the clarity of scripts
+mld_itp = LinearInterpolation((time_mld)days, mld)  
+
+# Create an idealized surface PAR timeseries
+time_par = [0.,20,120,200,330,364] # in days, piecewiselinear time nodes, must be float 
+par = [5,5,90,90,5,5] # in W/m^2
+PAR_itp = LinearInterpolation((time_par)days, par) #conduct linear interpolation to access to PAR at arbitrary time
+
+# Define surface_PAR as a function of time(in seconds). It will be used to update PAR profile as an auxiliary field
+surface_PAR(t) = PAR_itp(mod(t, 364days))  # the remainder of t after floored division by 364days. It creates an annual cycle representation of PAR. 
 
 # Simulation duration    
-duration=2days #2years    
+duration=100days #2years as another example    
 
 # Define the grid
 Lx = 1
@@ -53,20 +60,20 @@ Lz = 600 # domain depth
 
 grid = RectilinearGrid(
                 size=(Nx, Ny, Nz), 
-                extent=(Lx, Ly, Lz))
+                extent=(Lx, Ly, Lz)) #A regular rectilinear grid in this case. 
 
-#Initialize a PAR field
+# Initialize a PAR field
 PAR_field = Oceananigans.Fields.Field{Center, Center, Center}(grid)  
 
-#Set up the OceanBioME model with the specified biogeochemical model, light, and boundary conditions
-#If optional_sets, like carboates or oxygen, are not needed, just set optional_sets=(). The input of topboundaries has to be a NamedTuple.
-bgc = Setup.Oceananigans(:LOBSTER, grid, params, PAR_field, optional_sets=(), topboundaries=(none=nothing,))
+# Set up the OceanBioME model with the specified biogeochemical model, grid, parameters, light, and optional boundary conditions(More examples will be provided.)
+# If optional tracers are included, add e.g. optional_sets=(:carbonates, :oxygen), topboundaries=(DIC=dic_bc, OXY=oxy_bc). 
+bgc = Setup.Oceananigans(:LOBSTER, grid, params, PAR_field) 
 
 @info "Setup OceanBioME model"
-#Create a function of the turbulent vertical diffusivity. This is an idealized functional form, but the depth of mixing is based on an interpolation to the idealized mixed layer depth
+# Create a function of the turbulent vertical diffusivity. This is an idealized functional form, and the depth of mixing is based on an interpolation to the idealized mixed layer depth
 κₜ(x, y, z, t) = 1e-2*max(1-(z+mld_itp(mod(t,364days))/2)^2/(mld_itp(mod(t,364days))/2)^2,0)+1e-4; 
 
-# Now, create a 'model' to run in Oceananignas
+# Create a 'model' to run in Oceananignas
 model = NonhydrostaticModel(advection = UpwindBiasedFifthOrder(),
                             timestepper = :RungeKutta3,
                             grid = grid,
@@ -74,33 +81,32 @@ model = NonhydrostaticModel(advection = UpwindBiasedFifthOrder(),
                             coriolis = FPlane(f=1e-4),
                             buoyancy = BuoyancyTracer(), 
                             closure = ScalarDiffusivity(ν=κₜ, κ=κₜ), 
-                            forcing =  bgc.forcing,
+                            forcing = bgc.forcing,
                             boundary_conditions = bgc.boundary_conditions,
                             auxiliary_fields = (PAR=PAR_field, )
                             )
 
 # Initialize the biogeochemical variables
 # These initial conditions are set rather arbitrarily in the hope that the model will converge to a repeatable annual cycle if run long enough
-Pᵢ(x, y, z)= (tanh((z+250)/100)+1)/2*(0.038)+0.002         
-Zᵢ(x, y, z)= (tanh((z+250)/100)+1)/2*(0.038)+0.008          
-Dᵢ(x, y, z)=0
-DDᵢ(x, y, z)=0
-NO₃ᵢ(x, y, z)= (1-tanh((z+300)/150))/2*6+11.4  
-NH₄ᵢ(x, y, z)= (1-tanh((z+300)/150))/2*0.05+0.05      
-DOMᵢ(x, y, z)= 0 
+Pᵢ(x, y, z)= (tanh((z+250)/100)+1)/2*(0.038)+0.002           #in mmolN m^-3
+Zᵢ(x, y, z)= (tanh((z+250)/100)+1)/2*(0.038)+0.008           #in mmolN m^-3
+Dᵢ(x, y, z)=0                                                #in mmolN m^-3
+DDᵢ(x, y, z)=0                                               #in mmolN m^-3
+NO₃ᵢ(x, y, z)= (1-tanh((z+300)/150))/2*6+11.4                #in mmolN m^-3
+NH₄ᵢ(x, y, z)= (1-tanh((z+300)/150))/2*0.05+0.05             #in mmolN m^-3 
+DOMᵢ(x, y, z)= 0                                             #in mmolN m^-3  
 
-## set the initial conditions using functions or constants:
+# Set the initial conditions using functions or constants:
 set!(model, P=Pᵢ, Z=Zᵢ, D=Dᵢ, DD=DDᵢ, NO₃=NO₃ᵢ, NH₄=NH₄ᵢ, DOM=DOMᵢ, u=0, v=0, w=0, b=0)
 
-## Set up th simulation
+# Set up the simulation
 Δt=40 #timestep in second 
-
 simulation = Simulation(model, Δt=Δt, stop_time=duration) 
 
-# create a model 'callback' to update the light (PAR) profile every 1 timestep and integrate sediment model
-simulation.callbacks[:update_par] = Callback(Light.update_2λ!, IterationInterval(1), merge(params, (surface_PAR=surface_PAR,)))#
+# Create a model 'callback' to update the light (PAR) profile every 1 timestep and integrate sediment model
+simulation.callbacks[:update_par] = Callback(Light.update_2λ!, IterationInterval(1), merge(params, (surface_PAR=surface_PAR,)))
 
-## Print a progress message
+# Print a progress message
 progress_message(sim) = @printf("Iteration: %04d, time: %s, Δt: %s, wall time: %s\n",
                                 iteration(sim),
                                 prettytime(sim),
@@ -110,7 +116,7 @@ progress_message(sim) = @printf("Iteration: %04d, time: %s, Δt: %s, wall time: 
 # Create a message to display every 100 timesteps                                
 simulation.callbacks[:progress] = Callback(progress_message, IterationInterval(100))
 
-#setup dictionary of fields
+# Setup dictionary of fields
 fields = Dict(zip((["$t" for t in bgc.tracers]..., "PAR"), ([getproperty(model.tracers, t) for t in bgc.tracers]..., [getproperty(model.auxiliary_fields, t) for t in (:PAR, )]...)))
 simulation.output_writers[:profiles] = NetCDFOutputWriter(model, fields, filename="lobster_example.nc", schedule=TimeInterval(1days), overwrite_existing=true)
 
