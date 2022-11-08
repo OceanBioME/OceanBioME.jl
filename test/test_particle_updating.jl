ad4806bf224bf021cae3dc2a4ba111ce837b45ef
Jago <jagoosw@protonmail.com>
2022-11-08 15:39:04 +0000

    trying to fix tests
    Tests are definitly passing across a variety of places so no idea why won't pass here

--- test/test_particle_updating.jl
+++ test/test_particle_updating.jl
@@ -1,6 +1,5 @@
 using OceanBioME, Test, Oceananigans, StructArrays
 using OceanBioME.Particles: infinitesimal_particle_field_coupling!
-using Oceananigans: TendencyCallsite
 mutable struct CustomParticle
     #position
     x :: AbstractFloat
