b22cdd7bade538fdfe76ee8fed3dcce52b79e5f8
Jago Stong-Wright <jagoosw@protonmail.com>
2022-11-15 13:11:03 +0000

    Files were named wrong

--- src/Boundaries/gasexchange.jl
+++ src/Boundaries/gasexchange.jl
@@ -1,14 +1,4 @@
-#####
-##### Gas exchange model of [Wanninkhof1992](@cite)
-#####
-# TODO: Impliment Ho et al. 2006 wind speed dependence
-
-##### 
-##### Carbonate chemistry to determine pCO₂
-#####
-
 @inline CA_eq(H, params) = params.ALK - (params.KB/(params.KB + H))*params.Boron
-
 @inline H_eq(H, params) = CA_eq(H, params)*H^2 + params.K1*(CA_eq(H, params)-params.DIC)*H + params.K1*params.K2*(CA_eq(H, params)-2*params.DIC)
 
 function pCO₂(DIC, ALK, T, S, params)
@@ -25,18 +15,15 @@ function pCO₂(DIC, ALK, T, S, params)
 
     H = 10^(-params.pH) # initial guess from arg list
 
-    p=(DIC=DIC, ALK=ALK, K0=K0, K1=K1, K2=K2, KB=KB, Boron=Boron)
+    p = (DIC=DIC, ALK=ALK, K0=K0, K1=K1, K2=K2, KB=KB, Boron=Boron)
 
     H = find_zero(H_eq, H, atol=1e-100, p=p)
     CA = CA_eq(H, p)
-
-    CO2aq = CA/(K1/H + 2*K1*K2/H^2)*1e6 # Eq 11  μmol/kg
-    return CO2aq/K0 #ppm
+    
+    #pH = -log10(H)
+    CO2aq = 1e6*CA/(K1/H + 2*K1*K2/H^2) # Eq 11  μmol/kg
+    return CO2aq/K0 #μatm
 end
-
-#####
-##### Gas exchange velocity
-#####
 
 k(gas::Symbol, T, params)=0.39*(0.01/3600)*params.uₐᵥ^2*(Sc(T, getproperty(params.Sc_params, gas))/660)^(-0.5)#m/s, may want to add variable wind speed instead of average wind here at some point
 Sc(T, params) = params.A-params.B*T+params.C*T^2-params.D*T^3
@@ -47,16 +34,12 @@ Sc(T, params) = params.A-params.B*T+params.C*T^2-params.D*T^3
 #now fairly sure that this is giving the correct result for CO₂  as βρ is the henrys coefficient which sould be ∼34mol/m³ atm
 K(gas::Symbol, T, S, params)=k(gas, T, params)*β(T+273.15, S, getproperty(params.β_params, gas))*params.ρₒ #L=ρ\_wK₀ ->  https://reader.elsevier.com/reader/sd/pii/0304420374900152 and here K₀=β
 
-#####
-##### Air sea gas flux boundary functions
-#####
-
 function airseaflux(x, y, t, T::AbstractFloat, S::AbstractFloat, conc::AbstractFloat, params)
     #https://agupubs.onlinelibrary.wiley.com/doi/epdf/10.1029/92JC00188 could add chemical enhancement factor
     if params.gas in (:O₂, )#doing like this for flexability in the future
         return k(params.gas, T, params)*(conc-α(params.gas, T, S, params)*getproperty(params.conc_air, params.gas))
     elseif params.gas in (:CO₂, )
-        return K(params.gas, T, S, params)*(conc-getproperty(params.conc_air, params.gas))/(1000)#mol*ppmv/m^2*atm*s to mmolC/m²s not sure this is correct
+        return K(params.gas, T, S, params)*(conc-getproperty(params.conc_air, params.gas)*params.pAir)*1000#μatm/m²s to mmolC/m²s not sure this is correct
     else
         throw(ArgumentError("Invalid gas choice for airseaflux"))
     end
@@ -73,24 +56,8 @@ end
 airseaflux(x, y, t, conc, params) = airseaflux(x, y, t, params.T(x, y, 0, t), params.S(x, y, 0, t), conc, params)
 airseaflux(x, y, t, DIC, ALK, params) = airseaflux(x, y, t, DIC, ALK, params.T(x, y, 0, t), params.S(x, y, 0, t), params)
 
-#####
-##### Boundary condition setup
-#####
-"""
-    airseasetup(gas::Symbol; forcing=(T=nothing, S=nothing), parameters=defaults.airseaflux)
-
-Returns an Oceananigans `FluxBoundaryCondition` for the tracer relivant to the specified gas (i.e. DIC for CO₂ and oxygen for O₂).
-Arguments
-=========
-* `gas`: Symbol specifying the gas to exchange into the water, current choices are `:CO₂` and `:O₂`
-Keyword arguments
-=================
-* `forcing`: NamedTuple of functions in the form `func(x, y, z, t)` for temperature, `T`, and salinity `S`
-    Optional as the fallback is to look for tracer fields by these names (the prefered method where the physics are being resolved).
-* `parameters`: NamedTuple of parameters to replace the defaults
-"""
-
 function airseasetup(gas::Symbol; forcings=(T=nothing, S=nothing), parameters=defaults.airseaflux)
+    #don't like this but oh well
     if !(gas in keys(parameters.Sc_params))
         throw(ArgumentError("Boundary conditions not defined for gas $gas, available are $(keys(parameters.Sc_params))"))
     end
