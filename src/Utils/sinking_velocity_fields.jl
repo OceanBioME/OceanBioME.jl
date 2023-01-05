3fcc32b2bd8b46f0e59b6d764a03b18c68ab45ad
Jago Stong-Wright <jagoosw@protonmail.com>
2023-01-05 14:37:55 +0000

    Added error message when sinking_velocity_fields util is passed invalid values

--- src/Utils/sinking_velocity_fields.jl
+++ src/Utils/sinking_velocity_fields.jl
@@ -1,4 +1,4 @@
-using Oceananigans.Fields: ZFaceField
+using Oceananigans.Fields: ZFaceField, AbstractField
 using Oceananigans.Forcings: maybe_constant_field
 using Oceananigans.Grids: AbstractGrid
 
@@ -7,16 +7,14 @@ function setup_velocity_fields(drift_speeds, grid::AbstractGrid, open_bottom)
     for w in values(drift_speeds)
         u, v = maybe_constant_field.((0, 0))
         if isa(values(w), Number)
-            #if open_bottom
-            #    w = maybe_constant_field(-w)
-            #else
-                w_field = ZFaceField(grid)
-                for k=1:grid.Nz 
-                    @inbounds w_field[:, :, k] .= - w * ifelse(open_bottom, 1.0, (1 - exp((1-k)/2)))
-                end
-                w = w_field
-            #end
-        end # otherwise we give up and rely on the user
+            w_field = ZFaceField(grid)
+            for k=1:grid.Nz 
+                @inbounds w_field[:, :, k] .= - w * ifelse(open_bottom, 1.0, (1 - exp((1-k)/2)))
+            end
+            w = w_field
+        elseif !isa(values(w), Tuple{AbstractField, AbstractField, AbstractField})
+            error("Provided sinking speeds are an unsuitable value, must be a `NamedTuple` of scalar values (positive = sinking) with keys of tracer names, or `NamedTuple` of `VelocityFields`")
+        end
 
         push!(drift_velocities, (; u, v, w))
     end
