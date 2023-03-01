3e3075e4d6a7f9ed514927ce10fe597daa9b0258
Jago Stong-Wright <jagoosw@protonmail.com>
2023-03-01 19:16:13 +0000

    looks vaguly reasonable

--- test/test_sediments.jl
+++ test/test_sediments.jl
@@ -2,6 +2,8 @@ using OceanBioME, Oceananigans, Test
 using OceanBioME.Sediments: SimpleMultiG
 using Oceananigans.Units
 
+using CairoMakie
+
 function test_flat_sediment(architecture)
     grid = RectilinearGrid(architecture; size=(3, 3, 3), extent=(10, 10, 10))
 
@@ -13,7 +15,7 @@ function test_flat_sediment(architecture)
                                  boundary_conditions = (DIC = FieldBoundaryConditions(top = GasExchange(; gas = :CO₂)), 
                                                         O₂ = FieldBoundaryConditions(top = GasExchange(; gas = :O₂))),
                                  tracers = (:T, :S),
-                                 closure = ScalarDiffusivity(ν = 10 ^ -2, κ = 10 ^ -2))
+                                 closure = ScalarDiffusivity(ν = 10 ^ -3, κ = 10 ^ -3))
 
     set!(model.biogeochemistry.sediment_model.fields.N_fast, 30.0)
     set!(model.biogeochemistry.sediment_model.fields.N_slow, 30.0)
@@ -25,7 +27,7 @@ function test_flat_sediment(architecture)
          sPOC = model.biogeochemistry.organic_redfield, sPON = 1, bPOC = model.biogeochemistry.organic_redfield, bPON = 1,
          T = 20, S = 35)
 
-    simulation = Simulation(model, Δt = 300.0, stop_time = 50days)
+    simulation = Simulation(model, Δt = 700.0, stop_time = 500days)
 
     simulation.output_writers[:tracers] = JLD2OutputWriter(model, model.tracers,
                                                            filename = "sediment_test_tracers.jld2",
@@ -42,5 +44,52 @@ function test_flat_sediment(architecture)
     
     simulation.callbacks[:progress] = Callback(progress, IterationInterval(10))
 
+    scale_negative_tracers = ScaleNegativeTracers(tracers = (:NO₃, :NH₄, :P, :Z, :sPON, :bPON, :DON))
+    simulation.callbacks[:neg] = Callback(scale_negative_tracers; callsite = UpdateStateCallsite())
+    
+    plankton_redfield = model.biogeochemistry.phytoplankton_redfield
+    scale_negative_carbon_tracers = ScaleNegativeTracers(tracers = (:P, :Z, :DOC, :sPOC, :bPOC, :DIC), 
+                                                         scalefactors = (P = plankton_redfield, 
+                                                                         Z = plankton_redfield, 
+                                                                         DOC = 1, sPOC = 1, bPOC = 1, DIC = 1))
+    simulation.callbacks[:neg_carbon] = Callback(scale_negative_carbon_tracers; callsite = UpdateStateCallsite())
+
     return simulation, model
+end
+
+function load_timeseries(simulation)
+    tracer_timeseries = NamedTuple()
+    for tracer in keys(model.tracers)
+        if !(tracer in [:T, :S])
+            @info tracer
+            tracer_timeseries = merge(tracer_timeseries, NamedTuple{(tracer, )}((FieldTimeSeries(simulation.output_writers[:tracers].filepath, "$tracer")[2, 2, 1:3, 1:end], )))
+        end
+    end
+
+    sediment_timeseries = NamedTuple()
+    for tracer in keys(model.biogeochemistry.sediment_model.fields)
+        @info tracer
+        sediment_timeseries = merge(sediment_timeseries, NamedTuple{(tracer, )}((FieldTimeSeries(simulation.output_writers[:sediment].filepath, "$tracer")[2, 2, 1, 1:end], )))
+    end
+
+    return tracer_timeseries, sediment_timeseries
+end
+
+function plot_timeseries(simulation, tracer_timeseries, sediment_timeseries; times = FieldTimeSeries(simulation.output_writers[:tracers].filepath, "P").times)
+    fig = Figure(resolution = (1600, 1600))
+
+    for (idx, (name, values)) in enumerate(pairs(tracer_timeseries))
+        @info "$name"
+        ax = Axis(fig[idx % 5, ceil(Int, idx / 5) * 2 - 1], title = "$name", xlabel = "Time (days)", ylabel = "Depth (m)")
+        hm = heatmap!(ax, times ./ days, znodes(Center, simulation.model.grid), values')
+        Colorbar(fig[idx % 5, ceil(Int, idx / 5) * 2], hm, label = "$name Concentration (mmol / m³)")
+    end
+
+    for (idx, (name, values)) in enumerate(pairs(sediment_timeseries))
+        @info "$name"
+        ax = Axis(fig[(idx + length(tracer_timeseries)) % 5, ceil(Int, (idx + length(tracer_timeseries)) / 5) * 2 - 1], title = "$name", xlabel = "Time (days)", ylabel = "Concentration (mmol / m²)")
+        hm = lines!(ax, times ./ days, values)
+    end
+
+    return fig
 end
