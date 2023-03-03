f469d444f234e051695aee165a9e796c91c2cd53
Jago <jagoosw@protonmail.com>
2023-03-03 14:46:17 +0000

    added adapt structure for LOBSTER light attenuation

--- src/Models/AdvectedPopulations/LOBSTER/LOBSTER.jl
+++ src/Models/AdvectedPopulations/LOBSTER/LOBSTER.jl
@@ -403,7 +403,7 @@ adapt_structure(to, lobster::LOBSTER{FT, LA, B, W, A}) where {FT, LA, B, W, A} =
             lobster.fast_sinking_mortality_fraction,
             lobster.disolved_organic_breakdown_rate,
             lobster.zooplankton_calcite_dissolution,
-            lobster.light_attenuation_model,
+            adapt_structure(to, lobster.light_attenuation_model),
             lobster.optionals,
             NamedTuple{keys(lobster.sinking_velocities)}(ntuple(n -> adapt_structure(to, lobster.sinking_velocities[n]), length(lobster.sinking_velocities))), # not sure this is the most efficient way todo this
             NamedTuple{keys(lobster.advection_schemes)}(ntuple(n -> adapt_structure(to, lobster.advection_schemes[n]), length(lobster.advection_schemes))))
