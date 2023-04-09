9bf53c144fcba05451e22c62abfcfb03302d7b5f
Jago Stong-Wright <jagoosw@protonmail.com>
2023-04-08 23:02:44 -0400

    why not run now?

--- examples/eady.jl
+++ examples/eady.jl
@@ -76,7 +76,7 @@ set!(model, u=uᵢ, v=vᵢ, P = 0.03, Z = 0.03, NO₃ = 4.0, NH₄ = 0.05, DIC =
 simulation = Simulation(model, Δt = 15minutes, stop_time = 10days)
 
 # Adapt the time step while keeping the CFL number fixed
-wizard = TimeStepWizard(cfl=0.85, max_change = 1.5, max_Δt = 15minutes)
+wizard = TimeStepWizard(cfl=0.85, max_change = 1.5, max_Δt = 10minutes)
 simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
 
 # Create a progress message 
