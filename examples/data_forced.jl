b63778e7cb772ee951358e65a24f9a4d79768abf
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-05-26 14:44:43 +1000

    simpler plotting

--- examples/data_forced.jl
+++ examples/data_forced.jl
@@ -59,7 +59,7 @@ h(k) = (k - 1) / Nz
 Σ(k) = (1 - exp(-stretching * h(k))) / (1 - exp(-stretching))
 z_faces(k) = Lz * (ζ₀(k) * Σ(k) - 1)
 
-grid = RectilinearGrid(size = (1, 1, Nz), x = (0, 20)meters, y = (0, 20meters), z = z_faces)
+grid = RectilinearGrid(size = (1, 1, Nz), x = (0, 20meters), y = (0, 20meters), z = z_faces)
 
 # ## Biogeochemical and Oceananigans model
 # Here we instantiate the LOBSTER model with carbonate chemistry and a surface flux of DIC (CO₂)
@@ -81,7 +81,7 @@ set!(model, P = 0.03, Z = 0.03, NO₃ = 11.0, NH₄ = 0.05, DIC = 2200.0, Alk = 
 # - Prevent the tracers from going negative from numerical error (see discussion of this in the [positivity preservation](@ref pos-preservation) implimentation page)
 # - Adapt the timestep length to reduce the run time
 
-simulation = Simulation(model, Δt=1minutes, stop_time=100days) 
+simulation = Simulation(model, Δt = 1minutes, stop_time = 100days)
 
 progress_message(sim) = @printf("Iteration: %04d, time: %s, Δt: %s, wall time: %s\n",
                                 iteration(sim),
@@ -89,7 +89,7 @@ progress_message(sim) = @printf("Iteration: %04d, time: %s, Δt: %s, wall time: 
                                 prettytime(sim.Δt),
                                 prettytime(sim.run_wall_time))
 
-simulation.callbacks[:progress] = Callback(progress_message, IterationInterval(100))
+simulation.callbacks[:progress] = Callback(progress_message, IterationInterval(250))
 
 filename = "data_forced"
 simulation.output_writers[:profiles] = JLD2OutputWriter(model, 
@@ -112,50 +112,51 @@ run!(simulation)
 
 # Now we can visualise the results with some post processing to diagnose the air-sea CO₂ flux
 
-P = FieldTimeSeries("$filename.jld2", "P")
-NO₃ = FieldTimeSeries("$filename.jld2", "NO₃")
-Z = FieldTimeSeries("$filename.jld2", "Z")
+   P = FieldTimeSeries("$filename.jld2", "P")
+ NO₃ = FieldTimeSeries("$filename.jld2", "NO₃")
+   Z = FieldTimeSeries("$filename.jld2", "Z")
 sPOM = FieldTimeSeries("$filename.jld2", "sPOM") 
 bPOM = FieldTimeSeries("$filename.jld2", "bPOM")
-DIC = FieldTimeSeries("$filename.jld2", "DIC")
-Alk = FieldTimeSeries("$filename.jld2", "Alk")
+ DIC = FieldTimeSeries("$filename.jld2", "DIC")
+ Alk = FieldTimeSeries("$filename.jld2", "Alk")
 
 x, y, z = nodes(P)
 times = P.times
 
-air_sea_CO₂_flux = zeros(size(P)[4])
-carbon_export = zeros(size(P)[4])
+air_sea_CO₂_flux = carbon_export = zeros(length(times))
+
 for (i, t) in enumerate(times)
     air_sea_CO₂_flux[i] = CO₂_flux.condition.parameters(0.0, 0.0, t, DIC[1, 1, end, i], Alk[1, 1, end, i], t_function(1, 1, 0, t), s_function(1, 1, 0, t)) 
-    carbon_export[i] = model.biogeochemistry.organic_redfield * (sPOM[1, 1, end - 20, i] * model.biogeochemistry.sinking_velocities.sPOM.w[1, 1, end - 20] .+ bPOM[1, 1, end - 20, i] * model.biogeochemistry.sinking_velocities.bPOM.w[1, 1, end - 20])
+    carbon_export[i] = model.biogeochemistry.organic_redfield * (sPOM[1, 1, end-20, i] * model.biogeochemistry.sinking_velocities.sPOM.w[1, 1, end-20] .+ bPOM[1, 1, end-20, i] * model.biogeochemistry.sinking_velocities.bPOM.w[1, 1, end-20])
 end
 
 using CairoMakie
 
-f = Figure(backgroundcolor=RGBf(1, 1, 1), fontsize=30, resolution = (1920, 1600))
+f = Figure(backgroundcolor = RGBf(1, 1, 1), fontsize = 30, resolution = (1920, 1600))
 
-axP = Axis(f[1, 1:2], ylabel="z (m)", xlabel="Time (days)", title="Phytoplankton concentration (mmol N/m³)")
-hmP = heatmap!(times / days, float.(z[end-23:end]), float.(P[1, 1, end-23:end, 1:end])', colormap=:batlow)
+axis_kwargs = (xlabel="Time (days)", ylabel="z (m)", limits=((0, times[end] / days), (-150, 0)))
+
+axP = Axis(f[1, 1:2]; title="Phytoplankton concentration (mmol N/m³)", axis_kwargs...)
+hmP = heatmap!(times / days, z, interior(P, 1, 1, :, :)', colormap=:batlow)
 cbP = Colorbar(f[1, 3], hmP)
 
-axNO₃ = Axis(f[1, 4:5], ylabel="z (m)", xlabel="Time (days)", title="Nitrate concentration (mmol N/m³)")
-hmNO₃ = heatmap!(times / days, float.(z[end-23:end]), float.(NO₃[1, 1, end-23:end, 1:end])', colormap=:batlow)
+axNO₃ = Axis(f[1, 4:5]; title="Nitrate concentration (mmol N/m³)", axis_kwargs...)
+hmNO₃ = heatmap!(times / days, z, interior(NO₃, 1, 1, :, :)', colormap=:batlow)
 cbNO₃ = Colorbar(f[1, 6], hmNO₃)
 
-axZ = Axis(f[2, 1:2], ylabel="z (m)", xlabel="Time (days)", title="Zooplankton concentration (mmol N/m³)")
-hmZ = heatmap!(times / days, float.(z[end-23:end]), float.(Z[1, 1, end-23:end, 1:end])', colormap=:batlow)
+axZ = Axis(f[2, 1:2]; title="Zooplankton concentration (mmol N/m³)", axis_kwargs...)
+hmZ = heatmap!(times / days, z, interior(Z, 1, 1, :, :)', colormap=:batlow)
 cbZ = Colorbar(f[2, 3], hmZ)
 
-axD = Axis(f[2, 4:5], ylabel="z (m)", xlabel="Time (days)", title="Detritus concentration (mmol N/m³)")
-hmD = heatmap!(times./days, float.(z[end-23:end]), float.(sPOM[1, 1, end-23:end, 1:end])' .+ float.(bPOM[1, 1, end-23:end, 1:end])', colormap=:batlow)
+axD = Axis(f[2, 4:5]; title="Detritus concentration (mmol N/m³)", axis_kwargs...)
+hmD = heatmap!(times./days, z, interior(sPOM, 1, 1, :, :)' .+ interior(bPOM, 1, 1, :, :)', colormap=:batlow)
 cbD = Colorbar(f[2, 6], hmD)
 
-axfDIC = Axis(f[3, 1:6], xlabel="Time (days)", title="Air-sea CO₂ flux and Sinking", ylabel="Flux (kgCO₂/m²/year)")
-hmfDIC = lines!(times / days, cumsum(air_sea_CO₂_flux) .* (12 + 16 * 2) .* year / (1000 * 1000), label="Air-sea flux")
-hmfExp = lines!(times / days, cumsum(carbon_export) .* (12 + 16 * 2) .* year / (1000 * 1000), label="Sinking export")
+axfDIC = Axis(f[3, 1:6], xlabel="Time (days)", ylabel="Flux (kgCO₂/m²/year)", title="Air-sea CO₂ flux and Sinking")
+hmfDIC = lines!(times / days, cumsum(air_sea_CO₂_flux) * (12 + 16 * 2) * year / (1000 * 1000), label="Air-sea flux")
+hmfExp = lines!(times / days, cumsum(carbon_export) * (12 + 16 * 2) * year / (1000 * 1000), label="Sinking export")
 
 f[3, 5] = Legend(f, axfDIC, "", framevisible = false)
 
-save("data_forced.png", f)
-
-# ![Results](data_forced.png)
+current_figure() # hide
+fig
