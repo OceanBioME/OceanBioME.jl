63cf63aee6cdce073258f9da2ef0d4cdc39515dd
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-05-29 21:07:46 +1000

    import znodes + proper inbounds

--- src/Utils/timestep.jl
+++ src/Utils/timestep.jl
@@ -1,5 +1,5 @@
 using Oceananigans.Advection: cell_advection_timescaleᶜᶜᶜ
-using Oceananigans.Grids: Center, zspacing, minimum_zspacing
+using Oceananigans.Grids: Center, znodes, zspacing, minimum_zspacing
 
 @inline function column_diffusion_timescale(model)
     grid = model.grid
@@ -8,9 +8,9 @@ using Oceananigans.Grids: Center, zspacing, minimum_zspacing
 
     Δz2_ν = zeros(grid.Nz)
 
-    @inbounds for k in 1:grid.Nz
+    for k in 1:grid.Nz
         Δzₖ = zspacing(1, 1, k, grid, Center(), Center(), Center())
-        Δz2_ν[k] = Δzₖ^2 / model.closure.κ[1](0.0, 0.0, z[k], model.clock.time) # assumes all tracer closures are the same and x/y invariant
+        @inbounds Δz2_ν[k] = Δzₖ^2 / model.closure.κ[1](0.0, 0.0, z[k], model.clock.time) # assumes all tracer closures are the same and x/y invariant
     end
 
     return minimum(Δz2_ν)
