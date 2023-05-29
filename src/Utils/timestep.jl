f13d578e76cd7fcd522bd6b0094b60ab35324c9f
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-05-29 20:37:03 +1000

    missing imports + z, Δz inside loop

--- src/Utils/timestep.jl
+++ src/Utils/timestep.jl
@@ -1,16 +1,14 @@
 using Oceananigans.Advection: cell_advection_timescaleᶜᶜᶜ
-using Oceananigans.Grids: Center, znode, minimum_zspacing
+using Oceananigans.Grids: Center, znode, zspacing, minimum_zspacing
 
 @inline function column_diffusion_timescale(model)
     grid = model.grid
-
       t = model.clock.time
-     zₖ = znode(1, 1, k, grid, Center(), Center(), Center())
-    Δzₖ = zspacing(1, 1, k, grid, Center(), Center(), Center())
 
     Δz2_ν = zeros(grid.Nz)
-
     @inbounds for k in 1:grid.Nz
+         zₖ =    znode(1, 1, k, grid, Center(), Center(), Center())
+        Δzₖ = zspacing(1, 1, k, grid, Center(), Center(), Center())
         Δz2_ν[k] = Δzₖ^2 / model.closure.κ[1](0.0, 0.0, zₖ, t) # assumes all tracer closures are the same and x/y invariant
     end
 
