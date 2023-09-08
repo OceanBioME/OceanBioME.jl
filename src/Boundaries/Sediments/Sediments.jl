d9a210aa1a5237c4920b22bb6d97abdc1c9f443c
Jago Stong-Wright <jagoosw@protonmail.com>
2023-09-08 12:42:34 +0100

    maybe fixed

--- src/Boundaries/Sediments/Sediments.jl
+++ src/Boundaries/Sediments/Sediments.jl
@@ -37,7 +37,7 @@ function update_tendencies!(bgc, sediment::FlatSediment, model)
 
     launch!(arch, model.grid, :xy,
             _calculate_tendencies!,
-            bgc.sediment_model, bgc, model.grid, model.advection, model.tracers, model.timestepper.Gⁿ)
+            bgc.sediment, bgc, model.grid, model.advection, model.tracers, model.timestepper.Gⁿ)
             
     return nothing
 end
