6e8bb0c8da35183f01db9868a3bd269c4ea7db96
Jago Strong-Wright <jagoosw@protonmail.com>
2024-06-30 12:04:35 +0100

    reordered some things

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,10 @@ architecture = CUDA.has_cuda() ? GPU() : CPU()
 
 include("test_utils.jl")
 include("test_light.jl")
+include("test_slatissima.jl")
 include("test_LOBSTER.jl")
 include("test_NPZD.jl")
 include("test_gasexchange.jl")
-include("test_slatissima.jl")
 include("test_sediments.jl")
 
 architecture == CPU() && @testset "Doctests" begin
