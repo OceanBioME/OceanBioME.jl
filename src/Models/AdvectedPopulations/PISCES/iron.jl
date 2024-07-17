d34f68ff7dbe9151a9bfcb9f5f9c5874b2e046d7
coding-code123 <hannahmary.woods@outlook.com>
2024-07-17 15:05:47 +0100

    filled arguments and tracers

--- src/Models/AdvectedPopulations/PISCES/iron.jl
+++ src/Models/AdvectedPopulations/PISCES/iron.jl
@@ -1,8 +1,6 @@
 
 # TO DO: 
-    #Should Lₜ be defined as in (67) or from the parameter list? Best way to define this for Aggfe, should we define internally in Fe¹()?
-    #Where are θₘₐₓᶠᵉᵇᵃᶜᵗ?
-    #Where is K_Feᴮ¹ defined? where is θₘₐₓᶠᵉᵇᵃᶜᵗ defined?
+    #Fill in Bact function
 
 # Using simple chemistry model. 
 # This document contains functions for the following:
@@ -10,55 +8,94 @@
     # Cgfe1, Cgfe2, Aggfe, Bactfe (eqs 61, 62, 63)
     # Forcing for Fe (eq60)
 
-@inline function Fe¹(DOC, T, Fe)
+@inline function Fe¹(Fe, DOC, T)
     Lₜ = max(0.09*(DOC + 40) - 3, 0.6) # bgc.total_concentration_of_iron_ligands
-    K_eqᶠᵉ = 10^(16.27 - 1565.7/max(T, 5))
+    K_eqᶠᵉ = 10^(16.27 - 1565.7/max(T, 5)) #check this value
     Δ = 1 +  K_eqᶠᵉ(T)*Lₜ -  K_eqᶠᵉ(T)*Fₑ
 
-    return (-Δ + sqrt(Δ^2 + 4*K_eqᶠᵉ(T)*Fe))/2*K_eqᶠᵉ(T) #eq65
+    return (-Δ + sqrt(Δ^2 + 4*K_eqᶠᵉ(T)*Fe))/(2*K_eqᶠᵉ(T)) #eq65
 end
 
-@inline function Cgfe1(DOC, POC, Fe, T)
+@inline function Cgfe1(sh, Fe, POC, DOC, T)
     a₁ = bgc.aggregation_rate_of_DOC_to_POC_1
     a₂ = bgc.aggregation_rate_of_DOC_to_POC_2
     a₄ = bgc.aggregation_rate_of_DOC_to_POC_4
     a₅ = bgc.aggregation_rate_of_DOC_to_POC_5
    
-
-    FeL = Fe - Fe¹(DOC, T, Fe)
+    FeL = Fe - Fe¹(DOC, T, Fe) #eq64
     Fe_coll = 0.5*FeL
     return ((a₁*DOC + a₂*POC)*sh+a₄*POC + a₅*DOC)*Fe_coll
 end
 
-@inline function Cgfe2(GOC, Fe, T, DOC)
+@inline function Cgfe2(sh, Fe, T, DOC, GOC)
     a₃ = bgc.aggregation_rate_of_DOC_to_GOC_3
-    sh =
-
-    FeL = Fe - Fe¹(DOC, T, Fe)
+    FeL = Fe - Fe¹(Fe, DOC, T)
     Fe_coll = 0.5*FeL
     return a₃*GOC*sh*Fe_coll
 end
-@inline function Aggfe(Fe,DOC)
+
+@inline function Aggfe(Fe, DOC, T)
     λ_Fe = bgc.slope_of_scavenging_rate_of_iron
-    Lₜ = 
+    Lₜ = max(0.09*(DOC + 40) - 3, 0.6)
     return 1000*λ_Fe*max(0, Fe - Lₜ)*Fe¹(DOC, T, Fe)
 end
 
-@inline function (pisces::PISCES)(::Val{:Fe}, x, y, z, t, P, PAR) #(60)
-    σᶻ = bgc.non_assimilated_fraction[1]
-    γᴹ = bgc.excretion_as_DOM[2]
-    σᴹ = bgc.non_assimilated_fraction[2]
-    δᴾ = bgc.exudation_of_DOC[1]
-    δᴰ = bgc.exudation_of_DOC[2]
-   
-    sh = # Find value
-    θₘₐₓᶠᵉᵇᵃᶜᵗ = # check where is this defined?
+@inline function Bactfe(μₘₐₓ⁰, z, Z, M, Fe, DOC, PO₄, NO₃, NH₄, bFe, T, zₘₐₓ)
+    K_Feᴮ¹ = #add as parameter = 0.01 or 2.5e-10
+    θₘₐₓᶠᵉᵇᵃᶜᵗ = #add as parameter, 10e-6?
+    Bact = Bact(zₘₐₓ, z, Z, M) 
+    Lₗᵢₘᵇᵃᶜᵗ = Lᵇᵃᶜᵗ(DOC, PO₄, NO₃, NH₄, bFe)[2]
+    return μₘₐₓ⁰*fₚ(T)*Lₗᵢₘᵇᵃᶜᵗ*θₘₐₓᶠᵉᵇᵃᶜᵗ*Fe*Bact/(K_Feᴮ¹ + Fe) 
+end
+
+@inline function (pisces::PISCES)(::Val{:Fe}, x, y, z, t, P, D, Z, M, Pᶜʰˡ, Dᶜʰˡ, Pᶠᵉ, Dᶠᵉ, Dˢⁱ, DOC, POC, GOC, SFe, BFe, PSi, NO₃, NH₄, PO₄, Fe, Si, CaCO₃, DIC, O₂, T, PAR, PAR¹, PAR², PAR³, zₘₓₗ, zₑᵤ, Si̅) #eq60
+    σᶻ = bgc.non_assimilated_fraction.Z
+    γᴹ = bgc.excretion_as_DOM.M
+    σᴹ = bgc.non_assimilated_fraction.M
+    δᴾ = bgc.exudation_of_DOC.P
+    δᴰ = bgc.exudation_of_DOC.D
+    θᶠᵉᶻ = bgc.FeZ_redfield_ratio
+    μₘₐₓ⁰ = bgc.growth_rate_at_zero
+    θₘₐₓᶠᵉᴾ = 
+    Sᵣₐₜᴾ = 
+    K_Feᴾᶠᵉᵐⁱⁿ = 
+    Pₘₐₓ = 
+    L_Feᴾ = 
+    θₘₐₓᶠᵉᴰ = 
+    Sᵣₐₜᴰ = 
+    K_Feᴰᶠᵉᵐⁱⁿ = 
+    Iₘₐₓ = 
+    L_Feᴰ = 
+    
+    sh = get_sh(z, zₘₓₗ)
+    
     λₚₒ¹ = λ¹(T, O₂)
 
-    Bact = 
-    K_Feᴮ¹ = 
+    #Gross growth efficiency
+    eᶻ = eᴶ(eₘₐₓᶻ, σᶻ, gₚᶻ, g_Dᶻ, gₚₒᶻ, g_zᴹ, N, Fe, P, D, POC, Z)
+    eᴹ =  eᴶ(eₘₐₓᴹ, σᴹ, gₚᴹ, g_Dᴹ, gₚₒᴹ, g_zᴹ,Pᶠᵉ, Dᶠᵉ, SFe, P, D, POC)
 
-    Bactfe = μₚ()*Lₗᵢₘᵇᵃᶜᵗ()*θₘₐₓᶠᵉᵇᵃᶜᵗ*Fe*Bact/(K_Feᴮ¹ + Fe) # where is K_Feᴮ¹ defined? where is θₘₐₓᶠᵉᵇᵃᶜᵗ defined?
+    μᴾᶠᵉ = μᴵᶠᵉ(P, Pᶠᵉ, θₘₐₓᶠᵉᴾ, Sᵣₐₜᴾ, K_Feᴾᶠᵉᵐⁱⁿ, Pₘₐₓ, L_Feᴾ, bFe)
+    μᴰᶠᵉ = μᴵᶠᵉ(D, Dᶠᵉ, θₘₐₓᶠᵉᴰ, Sᵣₐₜᴰ, K_Feᴰᶠᵉᵐⁱⁿ, Iₘₐₓ, L_Feᴰ, bFe)
+   
+    #Iron quotas
+    θᶠᵉᴾ = θ(Pᶠᵉ, P)
+    θᶠᵉᴰ = θ(Dᶠᵉ, D)
+    θᶠᵉᴾᴼᶜ = θ(SFe, POC)
+    θᶠᵉᴳᴼᶜ = θ(BFe, GOC)
+    #Grazing
+    grazingᶻ = grazingᶻ(P, D, POC, T)
+    grazingᴹ = grazingᴹ(P, D, Z, POC, T)
+    ∑gᶻ = grazingᶻ[1]
+    ∑gᴹ = grazingᴹ[1]
+    ∑g_FFᴹ = ∑g_FFᴹ(zₑᵤ, zₘₓₗ, T, POC, GOC)
+    
 
-    return max(0, (1-σᶻ)*(∑θᶠᵉⁱ*gᶻ())/∑gᶻ() - eₙᶻ()θ(Zᶠᵉ, Z))*∑gᶻ()*Z + max(0, (1-σᴹ)*(∑θᶠᵉⁱ*gᴹ() + ∑θᶠᵉⁱ*g_FFᴹ())/(∑gᴹ()+g_FFᴹ()+g_FFᴹ()) - eₙᴹ()*θ(Zᶠᵉ, Z))*(∑gᴹ()+g_FFᴹ()+g_FFᴹ())*M + γᴹ*θ(Zᶠᵉ, Z)*Rᵤₚᴹ() + λ_poc1*SFe - (1 - δᴾ)*μᴾᶠᵉ()*P - (1 - δᴰ)*μᴰᶠᵉ()*D - Scav() - Cfge1() - Cgfe2() - Aggfe() - Bactfe()
+    ∑θᶠᵉⁱgᵢᶻ = θᶠᵉᴾ*grazingᶻ[2] + θᶠᵉᴰ*grazingᶻ[3] + θᶠᵉᴾᴼᶜ*grazingᶻ[4] #over P, D, POC
+    ∑θᶠᵉⁱgᵢᴹ = θᶠᵉᴾ*grazingᴹ[2] + θᶠᵉᴰ*grazingᴹ[3] + θᶠᵉᴾᴼᶜ*grazingᴹ[4] + θᶠᵉᶻ*grazingᴹ[5] #graze on P, D, POC, Z 
+
+    Bactfe = Bactfe(μₘₐₓ⁰, z, Z, M, Fe, DOC, PO₄, NO₃, NH₄, bFe, T, zₘₐₓ)
+
+    return max(0, (1-σᶻ)*(∑θᶠᵉⁱgᵢᶻ/∑gᶻ - eₙᶻ*θᶠᵉᶻ)*∑gᶻ*Z + max(0, (1-σᴹ)*(∑θᶠᵉⁱgᵢᴹ + θᶠᵉᴾᴼᶜ*gₚₒ_FF + θᶠᵉᴳᴼᶜ*g_GOC_FF )/(∑gᴹ+∑g_FFᴹ) - eₙᴹ*θᶠᵉᶻ)*(∑gᴹ+∑g_FFᴹ)*M + γᴹ*θᶠᵉᶻ*Rᵤₚᴹ(M, T) + λₚₒ¹*SFe - (1 - δᴾ)*μᴾᶠᵉ*P - (1 - δᴰ)*μᴰᶠᵉ*D 
+    - Scav(POC, GOC, CaCO₃, BSi, DOC, T, Fe) - Cgfe1(sh, Fe, POC, DOC, T) - Cgfe2(sh, Fe, T, DOC, GOC) - Aggfe(Fe, DOC, T) - Bactfe
 end
