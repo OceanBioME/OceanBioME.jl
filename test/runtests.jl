a450f2934fd4e730791febb24ab3d8f370571e2a
Jago Strong-Wright <jagoosw@protonmail.com>
2024-07-18 23:07:37 +0100

    fixed runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ include("test_light.jl")
 include("test_slatissima.jl")
 include("test_LOBSTER.jl")
 include("test_NPZD.jl")
-include("test_gasexchange.jl")
+include("test_gasexchange_carbon_chem.jl")
 include("test_sediments.jl")
 
 architecture == CPU() && @testset "Doctests" begin
