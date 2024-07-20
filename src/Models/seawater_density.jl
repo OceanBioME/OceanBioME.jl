a9e3c5123f975ae92ab0597be7417b4dbe3cfcd9
Jago Strong-Wright <jagoosw@protonmail.com>
2024-07-20 14:09:15 +0100

    unit issue for sw density

--- src/Models/seawater_density.jl
+++ src/Models/seawater_density.jl
@@ -4,10 +4,12 @@ using GibbsSeaWater: gsw_rho, gsw_sa_from_sp
     seawater_density(T, S, P=0)
 
 Returns sea water density computed by `GibbsSeaWater.jl` from
-`T` in degrees centigrade, `S` in PSU, and pressure in decibar.
+`T` in degrees centigrade, `S` in PSU, and pressure in bar.
 """
-@inline function seawater_density(T, Sp, P = 0, lon = 0, lat = 0)
-    Sa = gsw_sa_from_sp(Sp, P, lon, lat)
+@inline function seawater_density(T, Sp, Pbar = 0, lon = 0, lat = 0)
+    Pdbar = 10 * Pbar
+
+    Sa = gsw_sa_from_sp(Sp, Pdbar, lon, lat)
     
-    return gsw_rho(Sa, T, P)
+    return gsw_rho(Sa, T, Pdbar)
 end
