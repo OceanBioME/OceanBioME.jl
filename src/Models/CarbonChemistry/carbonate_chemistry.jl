e863822fc6d94fc0d1c55829b531ef285cac002e
Jago Strong-Wright <jagoosw@protonmail.com>
2024-07-20 15:30:54 +0100

    update docstring

--- src/Models/CarbonChemistry/carbonate_chemistry.jl
+++ src/Models/CarbonChemistry/carbonate_chemistry.jl
@@ -32,13 +32,13 @@ julia> carbon_chemistry = CarbonChemistry()
 Carbon chemistry model which solves for pCO₂ and pH
 
 julia> pCO₂ = carbon_chemistry(2000, 2000, 10, 35)
-1324.9372021360682
+1325.07184935965
 
 julia> pH = carbon_chemistry(2000, 2000, 10, 35; return_pH = true)
-7.4945380464223
+7.494548148653669
 
 julia> pCO₂_higher_pH = carbon_chemistry(2000, NaN, 10, 35, 7.5)
-1308.5503433443541
+1308.7134398483074
 
 ```
 """
