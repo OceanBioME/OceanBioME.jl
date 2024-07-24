89de5b5fe7490fe15427523a0b22da0fcd5f9902
ciadht <ct639@cam.ac.uk>
2024-07-24 13:28:52 +0100

    Latest initial conditions

--- src/Models/AdvectedPopulations/PISCES/boxPISCES.jl
+++ src/Models/AdvectedPopulations/PISCES/boxPISCES.jl
@@ -22,18 +22,25 @@ nothing #hide
 PAR⁰(t) = 60 * (1 - cos((t + 15days) * 2π / year)) * (1 / (1 + 0.2 * exp(-((mod(t, year) - 200days) / 50days)^2))) + 2
 
 z = -10 # specify the nominal depth of the box for the PAR profile
-PAR(t) = PAR⁰(t) * exp(0.2z) # Modify the PAR based on the nominal depth and exponential decay
+PAR(t) = 3000# Modify the PAR based on the nominal depth and exponential decay
+T(t) = 14.0
+
+#PAR¹(t) = 100.0
+#PAR²(t) = 100.0
+#PAR³(t) = 100.0
+#zₘₓₗ(t) = 100.0
+#zₑᵤ(t) = 50.0
 
 nothing #hide
 
 # Set up the model. Here, first specify the biogeochemical model, followed by initial conditions and the start and end times
-model = BoxModel(biogeochemistry = PISCES(grid = BoxModelGrid, light_attenuation_model = nothing), forcing = (; PAR))
+model = BoxModel(biogeochemistry = PISCES(grid = BoxModelGrid, light_attenuation_model = nothing), forcing = (; PAR, T)) #, PAR¹, PAR², PAR³, zₘₓₗ, zₑᵤ
 
-set!(model,NO₃ = 4.0, NH₄ = 0.1, P = 4.26, D = 4.26, Z = 0.426, M = 0.426, Pᶠᵉ = 3.5, Dᶠᵉ = 3.5, Pᶜʰˡ = 1.60, Dᶜʰˡ = 1.60, Dˢⁱ = 0.525, Fe = 0.8241, O₂ = 264.0, Si = 4.557, Alk = 2360.0, PO₄ = 0.8114)
+set!(model,NO₃ = 4.0, NH₄ = 0.1, P = .426, D = .426, Z = 0.426, M = 0.426, Pᶠᵉ = 3.5, Dᶠᵉ = 3.5, Pᶜʰˡ = 1.60, Dᶜʰˡ = 1.60, Dˢⁱ = 0.525, Fe = 0.8241, O₂ = 264.0, Si = 4.557, Alk = 2360.0, PO₄ = 0.8114, DIC = 2000.0, CaCO₃ = 1.0, PAR¹ = 1000.0, PAR² = 1000.0,  PAR³ = 1000.0, zₘₓₗ = 100.0, zₑᵤ = 50.0)
 
-simulation = Simulation(model; Δt = 0.5, stop_time = 3600.0)
+simulation = Simulation(model; Δt = 0.5, stop_time = 12hours)
 
-simulation.output_writers[:fields] = JLD2OutputWriter(model, model.fields; filename = "box.jld2", schedule = TimeInterval(1.0), overwrite_existing = true)
+simulation.output_writers[:fields] = JLD2OutputWriter(model, model.fields; filename = "box.jld2", schedule = TimeInterval(300), overwrite_existing = true)
 
 prog(sim) = @info "$(prettytime(time(sim))) in $(prettytime(simulation.run_wall_time))"
 
