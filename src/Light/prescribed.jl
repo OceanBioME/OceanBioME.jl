9066a7ab8cfeaee73b714748002b144c0558aab7
Jago Strong-Wright <jagoosw@protonmail.com>
2024-07-24 14:38:40 +0100

    added docstring

--- src/Light/prescribed.jl
+++ src/Light/prescribed.jl
@@ -1,7 +1,25 @@
-using Oceananigans.Fields: compute!
+using Oceananigans.Fields: compute!, AbstractField
 
-maybe_named_fields(field) = ((:PAR, ), (field, ))
+function maybe_named_fields(field)
+
+    isa(field, AbstractField) || @warn "fields: $field is not an `AbstractField"
+    
+    return ((:PAR, ), (field, ))
+end
+
 maybe_named_fields(fields::NamedTuple) = (keys(fields), values(fields))
+
+"""
+    PrescribedPhotosyntheticallyActiveRadiation(fields)
+
+`PrescribedPhotosyntheticallyActiveRadiation` returns "prescribed" PAR
+fields which are user specified, e.g. they may be `FunctionField`s or 
+`ConstantField`s.
+
+`fields` may either be an `AbstractField` or a `NamedTuple` of names and 
+fields which will be returned in `biogeochemical_auxiliary_fields`, if only
+one field is present the field will be named `PAR`.
+"""
 
 struct PrescribedPhotosyntheticallyActiveRadiation{F, FN}
     fields :: F
