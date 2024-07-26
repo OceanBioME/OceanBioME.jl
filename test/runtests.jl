90a6483eb804763749d746d90a4a4a45b759242f
Jago Strong-Wright <jagoosw@protonmail.com>
2024-07-26 12:57:08 +0100

    don't build on testgroup = none

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,21 @@
 include("dependencies_for_runtests.jl")
 
-include("test_utils.jl")
-include("test_light.jl")
-include("test_slatissima.jl")
-include("test_LOBSTER.jl")
-include("test_NPZD.jl")
-include("test_gasexchange.jl")
-include("test_sediments.jl")
+group = get(ENV, "TEST_GROUP", :all) |> Symbol
 
-if architecture == CPU() 
-    include("test_boxmodel.jl") # box models (probably) don't work on GPU, and it wouldn't be faster anyway
-end
+if group == :all
+    include("test_utils.jl")
+    include("test_light.jl")
+    include("test_slatissima.jl")
+    include("test_LOBSTER.jl")
+    include("test_NPZD.jl")
+    include("test_gasexchange.jl")
+    include("test_sediments.jl")
 
-architecture == CPU() && @testset "Doctests" begin
-    doctest(OceanBioME)
+    if architecture == CPU() 
+        include("test_boxmodel.jl") # box models (probably) don't work on GPU, and it wouldn't be faster anyway
+    end
+
+    architecture == CPU() && @testset "Doctests" begin
+        doctest(OceanBioME)
+    end
 end
