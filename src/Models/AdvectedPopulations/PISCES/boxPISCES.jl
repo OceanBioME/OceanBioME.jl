9295128dca97e13918191bd1b0493c66630f3209
ciadht <ct639@cam.ac.uk>
2024-07-26 10:16:10 +0100

    updated boxPISCES

--- src/Models/AdvectedPopulations/PISCES/boxPISCES.jl
+++ src/Models/AdvectedPopulations/PISCES/boxPISCES.jl
@@ -21,8 +21,8 @@ nothing #hide
 # This is forced by a prescribed time-dependent photosynthetically available radiation (PAR)
 PAR⁰(t) = 60 * (1 - cos((t + 15days) * 2π / year)) * (1 / (1 + 0.2 * exp(-((mod(t, year) - 200days) / 50days)^2))) + 2
 
-z = -10 # specify the nominal depth of the box for the PAR profile
-PAR(t) = 3000# Modify the PAR based on the nominal depth and exponential decay
+z = - 10 # specify the nominal depth of the box for the PAR profile
+PAR(t) = 300# Modify the PAR based on the nominal depth and exponential decay
 T(t) = 14.0
 
 #PAR¹(t) = 100.0
@@ -36,17 +36,16 @@ nothing #hide
 # Set up the model. Here, first specify the biogeochemical model, followed by initial conditions and the start and end times
 model = BoxModel(biogeochemistry = PISCES(grid = BoxModelGrid, light_attenuation_model = nothing), forcing = (; PAR, T)) #, PAR¹, PAR², PAR³, zₘₓₗ, zₑᵤ
 
-set!(model,NO₃ = 4.0, NH₄ = 0.1, P = .426, D = .426, Z = 0.426, M = 0.426, Pᶠᵉ = 3.5, Dᶠᵉ = 3.5, Pᶜʰˡ = 1.60, Dᶜʰˡ = 1.60, Dˢⁱ = 0.525, Fe = 0.8241, O₂ = 264.0, Si = 4.557, Alk = 2360.0, PO₄ = 0.8114, DIC = 2000.0, CaCO₃ = 1.0, PAR¹ = 1000.0, PAR² = 1000.0,  PAR³ = 1000.0, zₘₓₗ = 100.0, zₑᵤ = 50.0)
+set!(model,NO₃ = 4.0, NH₄ = 0.1, P = 4.26, D = 4.26, Z = 0.426, M = 0.426, Pᶠᵉ = 3.50, Dᶠᵉ = 3.50, Pᶜʰˡ = 1.60, Dᶜʰˡ = 1.60, Dˢⁱ = 0.525, Fe = 824.10, O₂ = 264.0, Si = 4.557, Alk = 2360.0, PO₄ = 0.8114, DIC = 2000.0, CaCO₃ = 1.0, PAR¹ = 100.0, PAR² = 100.0,  PAR³ = 100.0, zₘₓₗ = 100.0, zₑᵤ = 50.0)
 
-simulation = Simulation(model; Δt = 0.5, stop_time = 12hours)
+simulation = Simulation(model; Δt = 2.0, stop_time = 1day)
 
-simulation.output_writers[:fields] = JLD2OutputWriter(model, model.fields; filename = "box.jld2", schedule = TimeInterval(300), overwrite_existing = true)
+simulation.output_writers[:fields] = JLD2OutputWriter(model, model.fields; filename = "box.jld2", schedule = TimeInterval(10minutes), overwrite_existing = true)
 
 prog(sim) = @info "$(prettytime(time(sim))) in $(prettytime(simulation.run_wall_time))"
 
-simulation.callbacks[:progress] = Callback(prog, IterationInterval(10))
+simulation.callbacks[:progress] = Callback(prog, IterationInterval(100))
 
-# ## Run the model (should only take a few seconds)
 @info "Running the model..."
 run!(simulation)
 
