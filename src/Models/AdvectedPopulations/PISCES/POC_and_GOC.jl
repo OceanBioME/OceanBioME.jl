926d71f257653ea120452849be6038b416c01d99
coding-code123 <hannahmary.woods@outlook.com>
2024-08-09 11:22:24 +0100

    Commenting changes, and removed \theta^Sid factor

--- src/Models/AdvectedPopulations/PISCES/POC_and_GOC.jl
+++ src/Models/AdvectedPopulations/PISCES/POC_and_GOC.jl
@@ -1,6 +1,7 @@
-
+#Checked forcing equation, still to check parameters correctly named
 # This documeent contains functions for:
-    # Φ, POC, GOC
+    # Φ (eq39)
+    # POC, GOC (eqs37, 40)
 
 @inline function get_Φ(POC, GOC, sh, bgc)
     a₆ = bgc.aggregation_rate_of_POC_to_GOC_6
@@ -8,10 +9,11 @@
     a₈ = bgc.aggregation_rate_of_POC_to_GOC_8
     a₉ = bgc.aggregation_rate_of_POC_to_GOC_9
 
-    return sh*a₆*POC^2 + sh*a₇*POC*GOC + a₈*POC*GOC + a₉*POC^2  #39
+    return sh*a₆*POC^2 + sh*a₇*POC*GOC + a₈*POC*GOC + a₉*POC^2
 end
 
 @inline function (bgc::PISCES)(::Val{:POC}, x, y, z, t, P, D, Z, M, Pᶜʰˡ, Dᶜʰˡ, Pᶠᵉ, Dᶠᵉ, Dˢⁱ, DOC, POC, GOC, SFe, BFe, PSi, NO₃, NH₄, PO₄, Fe, Si, CaCO₃, DIC, Alk, O₂, T, zₘₓₗ, zₑᵤ, Si̅, D_dust, Ω, PAR, PAR¹, PAR², PAR³)
+    #Parameters
     σᶻ = bgc.non_assimilated_fraction.Z
     mᴾ, mᴰ = bgc.phytoplankton_mortality_rate
     mᶻ = bgc.zooplankton_quadratic_mortality.Z
@@ -22,23 +24,21 @@ end
     b_Z, bₘ = bgc.temperature_sensitivity_term
     g_FF = bgc.flux_feeding_rate
 
+    #Grazing
     grazing = get_grazingᶻ(P, D, POC, T, bgc)
     ∑gᶻ = grazing[1]
     gₚₒᶻ = grazing[4]
-
+    gₚₒᴹ = get_grazingᴹ(P, D, Z, POC, T, bgc)[4]
+    gₚₒ_FFᴹ = g_FF*(bₘ^T)*wₚₒ*POC #29a
+    
+    #Aggregation
     sh = get_sh(z, zₘₓₗ)
+    Φ₁ᴰᴼᶜ = Φᴰᴼᶜ(DOC, POC, GOC, sh, bgc)[1]
+    Φ₃ᴰᴼᶜ = Φᴰᴼᶜ(DOC, POC, GOC, sh, bgc)[3]
+    Φ = get_Φ(POC, GOC, sh, bgc)
 
     R_CaCO₃ = get_R_CaCO₃(P, PO₄, NO₃, NH₄, Pᶜʰˡ, Pᶠᵉ, Fe, T, PAR, zₘₓₗ, bgc) 
     λₚₒ¹ = λ¹(T, O₂, bgc)
-    Φ₁ᴰᴼᶜ = Φᴰᴼᶜ(DOC, POC, GOC, sh, bgc)[1]
-    Φ₃ᴰᴼᶜ = Φᴰᴼᶜ(DOC, POC, GOC, sh, bgc)[3]
-
-    gₚₒᴹ = get_grazingᴹ(P, D, Z, POC, T, bgc)[4]
-    gₚₒ_FFᴹ = g_FF*(bₘ^T)*wₚₒ*POC #29a
-    Φ = get_Φ(POC, GOC, sh, bgc)
-
-    #println("Total change in POC is ", (σᶻ*∑gᶻ*Z + 0.5*mᴰ*concentration_limitation(D, Kₘ)*D + rᶻ*(b_Z^T)*(concentration_limitation(Z, Kₘ) + 3*ΔO₂(O₂, bgc))*Z + mᶻ*(b_Z^T)*Z^2 + (1 - 0.5*R_CaCO₃)*(mᴾ*concentration_limitation(P, Kₘ)*P + sh*wᴾ*P^2) + λₚₒ¹*GOC + Φ₁ᴰᴼᶜ + Φ₃ᴰᴼᶜ - (gₚₒᴹ + gₚₒ_FFᴹ)*M - gₚₒᶻ*Z - λₚₒ¹*POC - Φ))
-    #println("Positive terms: a = $(σᶻ*∑gᶻ*Z), b = $(0.5*mᴰ*concentration_limitation(D, Kₘ)*D), c = $(rᶻ*(b_Z^T)*(concentration_limitation(Z, Kₘ) + 3*ΔO₂(O₂, bgc))*Z), " )
 
     return (σᶻ*∑gᶻ*Z + 0.5*mᴰ*concentration_limitation(D, Kₘ)*D + rᶻ*(b_Z^T)*(concentration_limitation(Z, Kₘ) + 3*ΔO₂(O₂, bgc))*Z 
             + mᶻ*(b_Z^T)*Z^2 + (1 - 0.5*R_CaCO₃)*(mᴾ*concentration_limitation(P, Kₘ)*P + sh*wᴾ*P^2) 
@@ -46,9 +46,9 @@ end
 end
 
 @inline function (bgc::PISCES)(::Val{:GOC}, x, y, z, t, P, D, Z, M, Pᶜʰˡ, Dᶜʰˡ, Pᶠᵉ, Dᶠᵉ, Dˢⁱ, DOC, POC, GOC, SFe, BFe, PSi, NO₃, NH₄, PO₄, Fe, Si, CaCO₃, DIC, Alk, O₂, T, zₘₓₗ, zₑᵤ, Si̅, D_dust, Ω, PAR, PAR¹, PAR², PAR³)
+    #Parameters
     σᴹ = bgc.non_assimilated_fraction.M
-    mᴾ = bgc.phytoplankton_mortality_rate.P
-    mᴰ = bgc.phytoplankton_mortality_rate.D
+    mᴾ, mᴰ = bgc.phytoplankton_mortality_rate
     wᴾ = bgc.min_quadratic_mortality_of_phytoplankton
     rᴹ = bgc.zooplankton_linear_mortality.M
     Kₘ = bgc.half_saturation_const_for_mortality
@@ -57,24 +57,25 @@ end
     wₘₐₓᴰ = bgc.max_quadratic_mortality_of_diatoms
     wₚₒ = bgc.sinking_speed_of_POC
 
+    #Grazing
+    w_GOC = get_w_GOC(z, zₑᵤ, zₘₓₗ, bgc)
     ∑gᴹ = get_grazingᴹ(P, D, Z, POC, T, bgc)[1] 
     ∑g_FFᴹ = get_∑g_FFᴹ(z, zₑᵤ, zₘₓₗ, T, POC, GOC, bgc)
-    
-    Pᵤₚᴹ = Pᵤₚ(M, T, bgc)
-    R_CaCO₃ = get_R_CaCO₃(P, PO₄, NO₃, NH₄, Pᶜʰˡ, Pᶠᵉ, Fe, T, PAR, zₘₓₗ, bgc)
-
-    sh = get_sh(z, zₘₓₗ)
-
-    Lₗᵢₘᴰ = D_nutrient_limitation(D, PO₄, NO₃, NH₄, Si, Dᶜʰˡ, Dᶠᵉ, Si̅, bgc)[1]
-    wᴰ =  wᴾ + wₘₐₓᴰ*(1-Lₗᵢₘᴰ)
-    Φ = get_Φ(POC, GOC, sh, bgc)
-    Φ₂ᴰᴼᶜ = Φᴰᴼᶜ(DOC, POC, GOC, sh, bgc)[2]
-    w_GOC = get_w_GOC(z, zₑᵤ, zₘₓₗ, bgc)
     gₚₒ_FFᴹ = g_FF*bₘ^T*wₚₒ*POC
     g_GOC_FFᴹ = g_FF*bₘ^T*w_GOC*GOC #29b
-    λₚₒ¹ = λ¹(T, O₂, bgc)
+    
+    #Aggregation
+    sh = get_sh(z, zₘₓₗ)
+    Φ = get_Φ(POC, GOC, sh, bgc)
+    Φ₂ᴰᴼᶜ = Φᴰᴼᶜ(DOC, POC, GOC, sh, bgc)[2]
 
-    return (σᴹ*(∑gᴹ + ∑g_FFᴹ)*M + rᴹ*bₘ^T*(concentration_limitation(M, Kₘ) + 3*ΔO₂(O₂, bgc))*M 
+    Pᵤₚᴹ = Pᵤₚ(M, T, bgc)
+    R_CaCO₃ = get_R_CaCO₃(P, PO₄, NO₃, NH₄, Pᶜʰˡ, Pᶠᵉ, Fe, T, PAR, zₘₓₗ, bgc)
+    λₚₒ¹ = λ¹(T, O₂, bgc)
+    Lₗᵢₘᴰ = D_nutrient_limitation(D, PO₄, NO₃, NH₄, Si, Dᶜʰˡ, Dᶠᵉ, Si̅, bgc)[1]
+    wᴰ =  wᴾ + wₘₐₓᴰ*(1-Lₗᵢₘᴰ)
+
+    return (σᴹ*(∑gᴹ + ∑g_FFᴹ)*M + rᴹ*(bₘ^T)*(concentration_limitation(M, Kₘ) + 3*ΔO₂(O₂, bgc))*M 
             + Pᵤₚᴹ + 0.5*R_CaCO₃*(mᴾ*concentration_limitation(P, Kₘ)*P + sh*wᴾ*P^2) + 0.5*mᴰ*concentration_limitation(D, Kₘ)*D
-            + sh*D^2*wᴰ + Φ + Φ₂ᴰᴼᶜ - g_GOC_FFᴹ*M - λₚₒ¹*GOC)     #40 
+            + sh*D^2*wᴰ + Φ + Φ₂ᴰᴼᶜ - g_GOC_FFᴹ*M - λₚₒ¹*GOC) 
 end
--- src/Models/AdvectedPopulations/PISCES/psi.jl
+++ src/Models/AdvectedPopulations/PISCES/psi.jl
@@ -1,5 +1,6 @@
+#Checked forcing equation, still to check parameters correctly named
 #This document contains functions for:
-    #λₚₛᵢ¹ (eq52, parametrisation of dissolution rate of PSi)
+    #λₚₛᵢ¹ (eq52, parametrisation of dissolution rate of PSi) 
     #Forcing for PSi (eq51)
 
 @inline function χ_lab(zₘₓₗ, zₑᵤ, λₚₛᵢˡᵃᵇ, λₚₛᵢʳᵉᶠ, z, bgc)
@@ -19,16 +20,16 @@ end
     λₚₛᵢˡᵃᵇ = bgc.fast_dissolution_rate_of_PSi
     λₚₛᵢʳᵉᶠ = bgc.slow_dissolution_rate_of_PSi
 
-    λₚₛᵢ = χ_lab(zₘₓₗ, zₑᵤ, λₚₛᵢˡᵃᵇ, λₚₛᵢʳᵉᶠ, z, bgc)*λₚₛᵢˡᵃᵇ + (1 - χ_lab(zₘₓₗ, zₑᵤ, λₚₛᵢˡᵃᵇ, λₚₛᵢʳᵉᶠ, z, bgc))*λₚₛᵢʳᵉᶠ
-
-    Si_eq = 10^(6.44 - 968/(T + 273.15))
+    Si_eq = 10^(6.44 - 968/(T + 273.15)) 
     Siₛₐₜ = (Si_eq - Si)/(Si_eq + eps(0.0))
 
-    return λₚₛᵢ*(0.225*(1 + T/15)*Siₛₐₜ + 0.775*(((1 + T/400)^4)*Siₛₐₜ)^9) #eq52
+    λₚₛᵢ = χ_lab(zₘₓₗ, zₑᵤ, λₚₛᵢˡᵃᵇ, λₚₛᵢʳᵉᶠ, z, bgc)*λₚₛᵢˡᵃᵇ + (1 - χ_lab(zₘₓₗ, zₑᵤ, λₚₛᵢˡᵃᵇ, λₚₛᵢʳᵉᶠ, z, bgc))*λₚₛᵢʳᵉᶠ
+
+    return λₚₛᵢ*(0.225*(1 + T/15)*Siₛₐₜ + 0.775*(((1 + T/400)^4)*Siₛₐₜ)^9) 
 end
 
 @inline function (bgc::PISCES)(::Val{:PSi}, x, y, z, t, P, D, Z, M, Pᶜʰˡ, Dᶜʰˡ, Pᶠᵉ, Dᶠᵉ, Dˢⁱ, DOC, POC, GOC, SFe, BFe, PSi, NO₃, NH₄, PO₄, Fe, Si, CaCO₃, DIC, Alk, O₂, T, zₘₓₗ, zₑᵤ, Si̅, D_dust, Ω, PAR, PAR¹, PAR², PAR³) 
-
+    #Parameters
     Kₘ = bgc.half_saturation_const_for_mortality
     wₘₐₓᴰ = bgc.max_quadratic_mortality_of_diatoms
     αᴰ = bgc.initial_slope_of_PI_curve.D
@@ -37,23 +38,12 @@ end
     mᴰ = bgc.phytoplankton_mortality_rate.D
     wᴾ = bgc.min_quadratic_mortality_of_phytoplankton
 
+    #Also required
     sh = get_sh(z, zₘₓₗ)
-
-    ϕ₀ = bgc.latitude
-    L_day_param = bgc.length_of_day
-    ϕ = get_ϕ(ϕ₀, y)
-    L_day = get_L_day(ϕ, t, L_day_param)
-
-    PARᴰ = get_PARᴰ(PAR¹, PAR², PAR³, bgc)
-
     Lₗᵢₘᴰ = D_nutrient_limitation(D, PO₄, NO₃, NH₄, Si, Dᶜʰˡ, Dᶠᵉ, Si̅, bgc)[1]
-
     wᴰ = wᴾ + wₘₐₓᴰ*(1-Lₗᵢₘᴰ)
-
-    μᴰ = phytoplankton_growth_rate(D, Dᶜʰˡ, PARᴰ, L_day, T, αᴰ, Lₗᵢₘᴰ, zₘₓₗ, zₑᵤ, t_darkᴰ, bgc)
-
     θˢⁱᴰ = θ(Dˢⁱ, D)
 
     return  (θˢⁱᴰ*get_grazingᴹ(P, D, Z, POC, T, bgc)[3]*M +  θˢⁱᴰ*get_grazingᶻ(P, D, POC, T, bgc)[3]*Z 
-           + θˢⁱᴰ*mᴰ*concentration_limitation(D, Kₘ)*Dˢⁱ + sh*wᴰ*D*Dˢⁱ - get_λₚₛᵢ¹(zₘₓₗ, zₑᵤ, z, T, Si, bgc)*Dissₛᵢ*PSi) #add partial derivative here
+           + mᴰ*concentration_limitation(D, Kₘ)*Dˢⁱ + sh*wᴰ*D*Dˢⁱ - get_λₚₛᵢ¹(zₘₓₗ, zₑᵤ, z, T, Si, bgc)*Dissₛᵢ*PSi) #removed θˢⁱᴰ from third term, to conserve silicon
 end
