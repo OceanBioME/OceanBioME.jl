580aca06c035672bea3fb40cde6b06b00d8fdb53
Jago Strong-Wright <jagoosw@protonmail.com>
2024-09-18 20:41:31 +0100

    guess we needed some of those things

--- src/Models/AdvectedPopulations/PISCES/mean_mixed_layer_properties.jl
+++ src/Models/AdvectedPopulations/PISCES/mean_mixed_layer_properties.jl
@@ -37,6 +37,10 @@ end
 #####
 ##### Mean mixed layer diffusivity
 #####
+compute_mean_mixed_layer_vertical_diffusivity!(κ::ConstantField, mixed_layer_depth, model) = nothing
+compute_mean_mixed_layer_vertical_diffusivity!(κ::ZeroField, mixed_layer_depth, model) = nothing
+compute_mean_mixed_layer_vertical_diffusivity!(κ::Nothing, mixed_layer_depth, model) = nothing
+
 compute_mean_mixed_layer_vertical_diffusivity!(κ, mixed_layer_depth, model) =
     compute_mean_mixed_layer_vertical_diffusivity!(model.closure, κ, mixed_layer_depth, model.diffusivity_fields, model.grid)
 
