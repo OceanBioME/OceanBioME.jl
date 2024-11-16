ef5fee6657a4b7ed7647d52fc7a28d2f74c0db46
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2024-11-15 20:06:51 -0700

    Need to import `TendencyCallsite`

--- test/test_sediments.jl
+++ test/test_sediments.jl
@@ -1,14 +1,12 @@
 include("dependencies_for_runtests.jl")
 
-using OceanBioME.Sediments: SimpleMultiG, InstantRemineralisation
 using Oceananigans.Units
 
-using OceanBioME.Sediments: sediment_tracers, sediment_fields
-using Oceananigans: Field
+using Oceananigans: Field, TendencyCallsite
 using Oceananigans.Fields: TracerFields
-
 using Oceananigans.Operators: volume, Azᶠᶜᶜ
 
+using OceanBioME.Sediments: SimpleMultiG, InstantRemineralisation, sediment_tracers, sediment_fields
 using OceanBioME.Models.LOBSTERModel: VariableRedfieldLobster
 
 function intercept_tracer_tendencies!(model, intercepted_tendencies)
@@ -23,22 +21,22 @@ function set_defaults!(sediment::SimpleMultiG)
 
     set!(sediment.fields.C_fast, 0.5893)
     set!(sediment.fields.C_slow, 0.1677)
-end 
+end
 
 set_defaults!(::InstantRemineralisation) = nothing
 
 set_defaults!(::LOBSTER, model) =
-    set!(model, P = 0.4686, Z = 0.5363, 
-                NO₃ = 2.3103, NH₄ = 0.0010, 
+    set!(model, P = 0.4686, Z = 0.5363,
+                NO₃ = 2.3103, NH₄ = 0.0010,
                 DOM = 0.8115,
                 sPOM = 0.2299, bPOM = 0.0103)
 
 
 set_defaults!(::VariableRedfieldLobster, model) =
-    set!(model, P = 0.4686, Z = 0.5363, 
-                NO₃ = 2.3103, NH₄ = 0.0010, 
-                DIC = 2106.9, Alk = 2408.9, 
-                O₂ = 258.92, 
+    set!(model, P = 0.4686, Z = 0.5363,
+                NO₃ = 2.3103, NH₄ = 0.0010,
+                DIC = 2106.9, Alk = 2408.9,
+                O₂ = 258.92,
                 DOC = 5.3390, DON = 0.8115,
                 sPON = 0.2299, sPOC = 1.5080,
                 bPON = 0.0103, bPOC = 0.0781)
@@ -46,8 +44,8 @@ set_defaults!(::VariableRedfieldLobster, model) =
 
 set_defaults!(::NutrientPhytoplanktonZooplanktonDetritus, model) =  set!(model, N = 2.3, P = 0.4, Z = 0.5, D = 0.2)
 
-total_nitrogen(sed::SimpleMultiG) = sum(sed.fields.N_fast) + 
-                                    sum(sed.fields.N_slow) + 
+total_nitrogen(sed::SimpleMultiG) = sum(sed.fields.N_fast) +
+                                    sum(sed.fields.N_slow) +
                                     sum(sed.fields.N_ref)
 
 total_nitrogen(sed::InstantRemineralisation) = sum(sed.fields.N_storage)
@@ -67,20 +65,20 @@ total_nitrogen(::VariableRedfieldLobster, model) = sum(model.tracers.NO₃) +
                                                      sum(model.tracers.DON) +
                                                      sum(model.tracers.sPON) +
                                                      sum(model.tracers.bPON)
-                                                     
+
 total_nitrogen(::NutrientPhytoplanktonZooplanktonDetritus, model) = sum(model.tracers.N) +
                                                                     sum(model.tracers.P) +
                                                                     sum(model.tracers.Z) +
                                                                     sum(model.tracers.D)
 
 function test_flat_sediment(grid, biogeochemistry, model; timestepper = :QuasiAdamsBashforth2)
-    model = isa(model, NonhydrostaticModel) ? model(; grid, 
-                                                      biogeochemistry, 
+    model = isa(model, NonhydrostaticModel) ? model(; grid,
+                                                      biogeochemistry,
                                                       closure = nothing,
                                                       timestepper,
                                                       buoyancy = nothing) :
-                                              model(; grid, 
-                                                      biogeochemistry, 
+                                              model(; grid,
+                                                      biogeochemistry,
                                                       closure = nothing,
                                                       buoyancy = nothing,
                                                       tracers = nothing)
@@ -114,7 +112,7 @@ function test_flat_sediment(grid, biogeochemistry, model; timestepper = :QuasiAd
 
     # conservations
     rtol = ifelse(isa(architecture, CPU), max(√eps(N₀), √eps(N₁)), 5e-7)
-    @test isapprox(N₀, N₁; rtol) 
+    @test isapprox(N₀, N₁; rtol)
 
     return nothing
 end
@@ -134,7 +132,7 @@ bottom_height(x, y) = -1000 + 500 * exp(- (x^2 + y^2) / 250) # a perfect hill
     grids = [RectilinearGrid(architecture; size=(3, 3, 50), extent=(10, 10, 500)),
              LatitudeLongitudeGrid(architecture; size = (3, 3, 16), latitude = (0, 10), longitude = (0, 10), z = (-500, 0)),
              ImmersedBoundaryGrid(
-                LatitudeLongitudeGrid(architecture; size = (3, 3, 16), latitude = (0, 10), longitude = (0, 10), z = (-500, 0)), 
+                LatitudeLongitudeGrid(architecture; size = (3, 3, 16), latitude = (0, 10), longitude = (0, 10), z = (-500, 0)),
                 GridFittedBottom(bottom_height))]
     for grid in grids
         for timestepper in (:QuasiAdamsBashforth2, :RungeKutta3),
@@ -142,15 +140,15 @@ bottom_height(x, y) = -1000 + 500 * exp(- (x^2 + y^2) / 250) # a perfect hill
             model in (NonhydrostaticModel, HydrostaticFreeSurfaceModel)
             for biogeochemistry in (NutrientPhytoplanktonZooplanktonDetritus(; grid, sediment_model),
                                     LOBSTER(; grid,
-                                              carbonates = ifelse(isa(sediment_model, SimpleMultiG), true, false), 
-                                              oxygen = ifelse(isa(sediment_model, SimpleMultiG), true, false), 
-                                              variable_redfield = ifelse(isa(sediment_model, SimpleMultiG), true, false), 
+                                              carbonates = ifelse(isa(sediment_model, SimpleMultiG), true, false),
+                                              oxygen = ifelse(isa(sediment_model, SimpleMultiG), true, false),
+                                              variable_redfield = ifelse(isa(sediment_model, SimpleMultiG), true, false),
                                               sediment_model))
                 # get rid of incompatible combinations
                 run = ifelse((model == NonhydrostaticModel && (isa(grid, ImmersedBoundaryGrid) || isa(grid, LatitudeLongitudeGrid))) ||
                              (model == HydrostaticFreeSurfaceModel && timestepper == :RungeKutta3) ||
                              (isa(sediment_model, SimpleMultiG) && isa(biogeochemistry.underlying_biogeochemistry, NutrientPhytoplanktonZooplanktonDetritus)), false, true)
-                
+
                 if run
                     @info "Testing sediment on $(typeof(architecture)) with $timestepper and $(display_name(sediment_model)) on $(display_name(biogeochemistry.underlying_biogeochemistry)) with $(display_name(grid))"
                     @testset "$architecture, $timestepper, $(display_name(sediment_model)), $(display_name(biogeochemistry.underlying_biogeochemistry)), $(display_name(grid))" test_flat_sediment(grid, biogeochemistry, model; timestepper)
