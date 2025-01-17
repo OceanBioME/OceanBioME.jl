22d016d725ac0a1bf6953455a012c8c7867b028e
Jago Strong-Wright <jagoosw@protonmail.com>
2025-01-17 14:54:15 +0000

    MARBL outline

---
+++ src/Models/AdvectedPopulations/MARBL/update_state.jl
@@ -0,0 +1,19 @@
+# computing diagnostic fields between timesteps 
+function update_biogeochemical_state!(model, bgc::MARBL)
+    # this should come from utils
+    #update_mixed_layer_depth!(bgc, model)
+
+    PAR = biogeochemical_auxiliary_fields(model.biogeochemistry.light_attenuation).PAR
+
+    #compute_euphotic_depth!(bgc.euphotic_depth, PAR)
+
+    #compute_mean_mixed_layer_vertical_diffusivity!(bgc.mean_mixed_layer_vertical_diffusivity, bgc.mixed_layer_depth, model)
+
+    #compute_mean_mixed_layer_light!(bgc.mean_mixed_layer_light, bgc.mixed_layer_depth, PAR, model)
+
+    #compute_calcite_saturation!(bgc.carbon_chemistry, bgc.calcite_saturation, model)
+
+    #update_silicate_climatology!(bgc, model)
+
+    return nothing
+end
