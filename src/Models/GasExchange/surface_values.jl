ea524b5686704170124b6cb38d77f4ce280a6cc9
Jago Strong-Wright <jagostrongwright@gmail.com>
2025-07-24 10:19:32 +0100

    Update surface_values.jl

--- src/Models/GasExchange/surface_values.jl
+++ src/Models/GasExchange/surface_values.jl
@@ -26,7 +26,8 @@ end
 @inline Adapt.adapt_structure(to, csf::DiscreteSurfaceFuncton) = 
     DiscreteSurfaceFuncton(adapt(to, csf.func))
 
-@inline surface_value(f::DiscreteSurfaceFuncton, i, j, grid, clock, args...) = f.func(i, j, grid, clock, args...)
+@inline surface_value(f::DiscreteSurfaceFuncton, i, j, grid, clock, args...) = 
+    f.func(i, j, grid, clock, args...)
 
 # GPU compatible, mainly for fields
 @inline surface_value(f::AbstractArray{<:Any, 2}, i, j, grid, clock, args...) = @inbounds f[i, j]
