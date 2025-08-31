616cf8ac4fee3b17a26b6b8da8320c87ea195497
johnryantaylor <johnryantaylor@gmail.com>
2025-08-31 12:42:52 +0100

    created PISCES profiling script

--- profiling/PISCES_3D.jl
+++ profiling/PISCES_3D.jl
@@ -1,16 +1,17 @@
-# This script is intended as a profiling example for the LOBSTER biogeochemical model
-# and how it can be used with active particles to model the growth of sugar kelp.
-# It is intended as a typical use example for the OceanBioME.jl package.
+# This script is intended as a profiling example for the PISCES biogeochemical model
 
 # ## Model setup
-# We load the required packages. 
 using OceanBioME, Oceananigans, Printf
+
+using Oceananigans.Fields: FunctionField, ConstantField
 using Oceananigans.Units
 using Random
-using JLD2
-using Plots
-using OceanBioME.Particles: BiogeochemicalParticles
-import OceanBioME.Particles: required_particle_fields, required_tracers, coupled_tracers
+using OceanBioME.Sediments: sinking_flux
+
+using Plots # for plotting
+
+const year = years = 365days
+nothing #hide
 
 # Set the architecture to use.
 arch = CPU()
@@ -22,7 +23,7 @@ plot_results = true # Set to `false` to not plot the results at the end of the s
 
 duration = 10days # Duration of the simulation
 
-filename = "LOBSTER" # Base filename for output files
+filename = "PISCES" # Base filename for output files
 
 # set the number of gridpoints in each direction
 Nx = 32
@@ -36,6 +37,8 @@ Lz = 100meters
 
 # Construct a grid with uniform grid spacing.
 grid = RectilinearGrid(arch, size = (Nx, Ny, Nz), extent = (Lx, Ly, Lz))
+
+clock = Clock(; time = 0.0)
 
 # Set the Coriolis and buoyancy models.
 coriolis = FPlane(f = 1e-4) # [s⁻¹]
@@ -53,51 +56,52 @@ background_state_parameters = (M = 1e-4,       # s⁻¹, geostrophic shear
 # gradient (in the zonal direction). The background velocity components ``U`` and ``V`` are prescribed
 # so that the thermal wind relationship is satisfied, that is, ``f \partial_z U = - \partial_y B`` and
 # ``f \partial_z V = \partial_x B``.
-T(x, y, z, t, p) = (p.M^2 * x + p.N^2 * (z + p.H)) / (p.g * p.α)
-V(x, y, z, t, p) = p.M^2 / p.f * (z + p.H)
+T_func(x, y, z, t, p) = (p.M^2 * x + p.N^2 * (z + p.H)) / (p.g * p.α)
+V_func(x, y, z, t, p) = p.M^2 / p.f * (z + p.H)
 
-V_field = BackgroundField(V, parameters = background_state_parameters)
-T_field = BackgroundField(T, parameters = background_state_parameters)
+V_field = BackgroundField(V_func, parameters = background_state_parameters)
+T_field = BackgroundField(T_func, parameters = background_state_parameters)
 
-# Specify some horizontal and vertical viscosity and diffusivity.
-νᵥ = κᵥ = 1e-4 # [m² s⁻¹]
+# Specify a vertical viscosity and diffusivity.
+νᵥ = κᵥ = 1e-2 # [m² s⁻¹]
 vertical_diffusivity = VerticalScalarDiffusivity(ν = νᵥ, κ = κᵥ)
 
+# ## Surface PAR and turbulent vertical diffusivity based on idealised mixed layer depth
+# Setting up idealised functions for PAR and diffusivity 
 
-# ## Kelp Particle setup
-n = 5 # number of kelp bundles
-z₀ = [-21:5:-1;] * 1.0 # depth of kelp fronds
-particles = SugarKelpParticles(n; grid, 
-                               scalefactors = fill(2000, n)) # and we want them to look like there are 500 in each bundle
-# Initial conditions for the kelp particles.
-set!(particles, A = 10, N = 0.01, C = 0.1, z = z₀, x = Lx / 2, y = Ly / 2)
+@inline PAR⁰(x, y, t) = 20.0
 
-# Setup the biogeochemical model with optional carbonate chemistry turned on.
-biogeochemistry = LOBSTER(; grid,
-                            carbonates = true,
-                            oxygen = true,
-                            variable_redfield = true,
-                            scale_negatives = true,
-                            open_bottom = true,
-                            particles)
+MLD = -100.0
 
-DIC_bcs = FieldBoundaryConditions(top = CarbonDioxideGasExchangeBoundaryCondition())
+@inline κₜ(x, y, z, t) = (1e-2 * (1 + tanh((z - MLD) / 10)) / 2 + 1e-4)
 
-# Model instantiation
+κ_field = FunctionField{Center, Center, Center}(κₜ, grid; clock)
+
+# Model setup
+carbon_chemistry = CarbonChemistry()
+
+biogeochemistry = PISCES(; grid, 
+                           mixed_layer_depth = -100.0,
+                           mean_mixed_layer_vertical_diffusivity = ConstantField(1e-2), # this is by default computed now
+                           surface_photosynthetically_active_radiation = PAR⁰,
+                           carbon_chemistry)
+
+CO₂_flux = CarbonDioxideGasExchangeBoundaryCondition(; carbon_chemistry)
+O₂_flux = OxygenGasExchangeBoundaryCondition()
+
+@info "Setting up the model..."
 model = NonhydrostaticModel(; grid,
-                              biogeochemistry,
-                              boundary_conditions = (DIC = DIC_bcs, ),
-                              advection = WENO(),
-                              timestepper = :RungeKutta3,
-                              coriolis,
-                              tracers = (:T, :S),
-                              buoyancy,
-                              background_fields = (T = T_field, v = V_field),
-                              closure = vertical_diffusivity)
+                                      advection = WENO(),
+                                      buoyancy,
+                                      coriolis,
+                                      clock,
+                                      closure = ScalarDiffusivity(VerticallyImplicitTimeDiscretization(), κ = κ_field),
+                                      biogeochemistry,
+                                      background_fields = (T = T_field, v = V_field))
+                                      #boundary_conditions = (DIC = FieldBoundaryConditions(top = CO₂_flux), O₂ = FieldBoundaryConditions(top = O₂_flux)))
 
-# ## Initial conditions
-# Start with a bit of random noise added to the background thermal wind and an arbitary
-# biogeochemical state.
+
+@info "Setting initial values..."
 
 # Although not required, we also set the random seed to ensure reproducibility of the results.
 Random.seed!(11)
@@ -108,33 +112,34 @@ Ũ = 1e-3
 uᵢ(x, y, z) = Ũ * Ξ(z)
 vᵢ(x, y, z) = Ũ * Ξ(z)
 
-set!(model, u=uᵢ, v=vᵢ, P = 0.03, Z = 0.03, NO₃ = 4.0, NH₄ = 0.05, DIC = 2200.0, Alk = 2409.0, S = 35, T = 20)
-
-# ## Setup the simulation
-# Choose an appropriate initial timestep for this resolution and set up the simulation
+set!(model, u = uᵢ, v=vᵢ, P = 0.1, PChl = 0.025, PFe = 0.005,
+            D = 0.01, DChl = 0.003, DFe = 0.0006, DSi = 0.004,
+            Z = 0.06, M = 0.5,
+            DOC = 0.5,
+            POC = 5.4, SFe = 0.34,
+            GOC = 8.2, BFe = 0.5, PSi = 0.04, CaCO₃ = 10^-10,
+            NO₃ = 10, NH₄ = 0.1, PO₄ = 5.0, Fe = 0.6, Si = 8.6,
+            DIC = 2205, Alk = 2560, O₂ = 317, T=20, S = 35)
 
 Δx = minimum_xspacing(grid, Center(), Center(), Center())
 Δy = minimum_yspacing(grid, Center(), Center(), Center())
 Δz = minimum_zspacing(grid, Center(), Center(), Center())
 
-Δt₀ = 0.75 * min(Δx, Δy, Δz) / V(0, 0, 0, 0, background_state_parameters)
+Δt₀ = 0.75 * min(Δx, Δy, Δz) / V_func(0, 0, 0, 0, background_state_parameters)
 
 simulation = Simulation(model, Δt = Δt₀, stop_time = duration)
 
 # Adapt the time step while keeping the CFL number fixed.
 wizard = TimeStepWizard(cfl = 0.75, diffusive_cfl = 0.75, max_Δt = 30minutes)
 simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(5))
-nothing #hide
 
-# Create a progress message.
-progress(sim) = @printf("i: % 6d, sim time: % 10s, wall time: % 10s, Δt: % 10s, CFL: %.2e\n",
-                        sim.model.clock.iteration,
-                        prettytime(sim.model.clock.time),
-                        prettytime(sim.run_wall_time),
-                        prettytime(sim.Δt),
-                        AdvectiveCFL(sim.Δt)(sim.model))
+progress_message(sim) = @printf("Iteration: %04d, time: %s, Δt: %s, wall time: %s\n",
+                                iteration(sim),
+                                prettytime(sim),
+                                prettytime(sim.Δt),
+                                prettytime(sim.run_wall_time))
 
-simulation.callbacks[:progress] = Callback(progress, IterationInterval(20))
+add_callback!(simulation, progress_message, IterationInterval(1))
 
 # Here, we add some diagnostics to calculate and output.
 u, v, w = model.velocities # unpack velocity `Field`s
@@ -142,28 +147,18 @@ u, v, w = model.velocities # unpack velocity `Field`s
 # and also calculate the vertical vorticity.
 ζ = Field(∂x(v) - ∂y(u))
 
-# Periodically save the velocities and vorticity to a file.
+#add_callback!(simulation, update_temperature!, IterationInterval(1))
+# Periodically save the tracers, velocities and vorticity to a file.
 simulation.output_writers[:fields] = JLD2Writer(model, merge(model.tracers, (; u, v, w, ζ));
-                                                      schedule = TimeInterval(2hours),
+                                                      schedule = TimeInterval(1hour),
                                                       filename = "$(filename)_bgc.jld2",
                                                       overwrite_existing = true)
-
-import OceanBioME.Particles: fetch_output                                                      
-function fetch_output(particles::BiogeochemicalParticles, model)
-    return particles
-end
-
-simulation.output_writers[:particles] = JLD2Writer(model, (; model.biogeochemistry.particles),
-                                                         filename = "$(filename)_particles.jld2",
-                                                         schedule = TimeInterval(2hours),
-                                                         overwrite_existing = true)                                                      
-
-nothing #hide
-
-# Run the simulation
+# ## Run!
+# We are ready to run the simulation
 run!(simulation)
 
 if plot_results
+    using JLD2, Plots, Statistics
 
     # Load the saved output,
     ζ = FieldTimeSeries("$(filename)_bgc.jld2", "ζ")
@@ -171,15 +166,26 @@ if plot_results
     NO₃ = FieldTimeSeries("$(filename)_bgc.jld2", "NO₃")
     NH₄ = FieldTimeSeries("$(filename)_bgc.jld2", "NH₄")
     DIC = FieldTimeSeries("$(filename)_bgc.jld2", "DIC")
+    Alk = FieldTimeSeries("$(filename)_bgc.jld2", "Alk")
+    T = FieldTimeSeries("$(filename)_bgc.jld2", "T")
+    S = FieldTimeSeries("$(filename)_bgc.jld2", "S")
 
-    file_particles = jldopen("$(filename)_particles.jld2")
-    iterations = keys(file_particles["timeseries/t"])
+    file = jldopen("$(filename)_bgc.jld2")
+    iterations = keys(file["timeseries/t"])
 
     times = ζ.times
 
     xζ, yζ, zζ = nodes(ζ)
     xc, yc, zc = nodes(P)
     nothing #hide
+
+    Pcolumn = zeros(length(zc), length(times))
+    NO₃column = zeros(length(zc), length(times))
+    DICcolumn = zeros(length(zc), length(times))
+    NH₄column = zeros(length(zc), length(times))
+    Alkcolumn = zeros(length(zc), length(times))
+    Tcolumn = zeros(length(zc), length(times))
+    Scolumn = zeros(length(zc), length(times))
 
     # loop over all saved times and make an animation
     anim = @animate for (i, iter) in enumerate(iterations[1:end])
@@ -191,10 +197,16 @@ if plot_results
         Pₙ = interior(  P[i], :, :, grid.Nz)
         DICₙ = interior(DIC[i], :, :, grid.Nz)
 
-        # read in particle data at current timestep
-        particles = file_particles["timeseries/particles/$iter"]
+        # extract a column
+        Pcolumn[:,i] = interior(P[i], 1, 1, :)
+        NO₃column[:,i] = interior(NO₃[i], 1, 1, :)
+        DICcolumn[:,i] = interior(DIC[i], 1, 1, :)
+        NH₄column[:,i] = interior(NH₄[i], 1, 1, :)
+        Alkcolumn[:,i] = interior(Alk[i], 1, 1, :)
+        Tcolumn[:,i] = interior(T[i], 1, 1, :)
+        Scolumn[:,i] = interior(S[i], 1, 1, :)
 
-        lims = [(minimum(T), maximum(T)) for T in (  ζ[:, :, grid.Nz, :],
+        lims = [(minimum(test), maximum(test)) for test in (  ζ[:, :, grid.Nz, :],
                                            NO₃[:, :, grid.Nz, :] .+ NH₄[:, :, grid.Nz, :],
                                              P[:, :, grid.Nz, :],
                                            DIC[:, :, grid.Nz, :])]
@@ -219,7 +231,6 @@ if plot_results
 end
 nothing #hide
 
-mp4(anim, "LOBSTER.mp4", fps = 15) # hide
+mp4(anim, "PISCES.mp4", fps = 15) # hide
 
 end # end if plot_results
-
