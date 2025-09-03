b824d8db1f2d62aa65ebe7b6f049fe3b25caf74b
Adeleke Bankole <ab3191@login-p-1.data.cluster>
2025-09-03 12:28:31 +0100

    update scaling_run script with PISCES data

--- profiling/scaling_run.jl
+++ profiling/scaling_run.jl
@@ -22,7 +22,7 @@ function run_benchmark_case(
     @info "Precompiling benchmark case again"
     @time Profiling.Cases.big_LOBSTER(; backend, grid_size, n_particles, enable_io, fast_kill = true, runlength_scale, filename)
 
-    @info "Precompiling bemchmark case yet again!"
+    @info "Precompiling benchmark case yet again!"
     @time Profiling.Cases.big_LOBSTER(;backend, grid_size, n_particles, enable_io, fast_kill = true, runlength_scale, filename)
 
 
@@ -33,6 +33,37 @@ function run_benchmark_case(
         # Remove the output if it exists
         rm("$(filename)_bgc.jld2")
         rm("$(filename)_particles.jld2")
+    end
+
+    return run_data # Could be removed but let's be explicit about returned value
+end
+
+function run_PISCES_benchmark_case(
+        backend;
+        enable_io::Bool,
+        grid_size::Tuple{Int, Int, Int},
+        runlength_scale::Float64,
+    )
+    @info "Running benchmark case with grid size: $grid_size end with IO: $enable_io"
+    filename = "SCALING_RUN_GPU"
+    @info "Using output name: $filename"
+
+    @info "Precompiling benchmark case"
+    @time Profiling.Cases.big_PISCES(; backend, grid_size, enable_io, fast_kill = true, runlength_scale, filename)
+
+    @info "Precompiling benchmark case again"
+    @time Profiling.Cases.big_PISCES(; backend, grid_size, enable_io, fast_kill = true, runlength_scale, filename)
+
+    @info "Precompiling benchmark case yet again!"
+    @time Profiling.Cases.big_PISCES(; backend, grid_size, enable_io, fast_kill = true, runlength_scale, filename)
+
+
+    @info "Running the case"
+    run_data = @timed Profiling.Cases.big_PISCES(; backend, grid_size, enable_io, fast_kill = false, runlength_scale, filename)
+
+    if enable_io
+        # Remove the output if it exists
+        rm("$(filename)_bgc.jld2")
     end
 
     return run_data # Could be removed but let's be explicit about returned value
@@ -81,6 +112,35 @@ function repackge_run_data(run_data, run_parameters)
     )
 end
 
+function repackge_PISCES_run_data(run_data, run_parameters)
+    (;
+        # Run parameters
+        grid_cells = prod(run_parameters.grid_size),
+        Nx = run_parameters.grid_size[1],
+        Ny = run_parameters.grid_size[2],
+        Nz = run_parameters.grid_size[3],
+        io = run_parameters.enable_io,
+        runlength_scale = run_parameters.runlength_scale,
+
+        # Runtime data
+        elapsed_time = run_data.time,
+        bytes_allocated = run_data.bytes,
+        gc_time = run_data.gctime,
+        compile_time = run_data.compile_time,
+        recompile_time = run_data.recompile_time,
+
+        # Garbage collector data
+        gc_bytes_allocated = run_data.gcstats.allocd,
+        gc_number_of_pauses = run_data.gcstats.pause,
+        gc_number_of_malloc_calls = run_data.gcstats.malloc,
+        gc_number_of_ralloc_calls = run_data.gcstats.realloc,
+        gc_number_of_pool_allocations = run_data.gcstats.poolalloc,
+        gc_number_of_big_nonpool_allocations = run_data.gcstats.bigalloc,
+        gc_number_of_free_calls = run_data.gcstats.freecall,
+        gc_number_of_full_sweeps = run_data.gcstats.full_sweep
+    )
+end
+
 
 backend = GPU()
 
@@ -101,11 +161,34 @@ cases_grid = [
     (grid_size = (256, 256, 128), n_particles = 5, enable_io = true, runlength_scale = 1.0),
 ]
 
+cases_PISCES_grid = [
+    (grid_size = (16, 16, 8), enable_io = true, runlength_scale = 1.0),
+    (grid_size = (16, 32, 8), enable_io = true, runlength_scale = 1.0),
+    (grid_size = (32, 32, 8), enable_io = true, runlength_scale = 1.0), # Nominal Case
+    (grid_size = (32, 32, 16), enable_io = true, runlength_scale = 1.0),
+    (grid_size = (64, 32, 16), enable_io = true, runlength_scale = 1.0),
+    (grid_size = (64, 64, 16), enable_io = true, runlength_scale = 1.0),
+    (grid_size = (64, 64, 32), enable_io = true, runlength_scale = 1.0),
+    (grid_size = (128, 64, 32), enable_io = true, runlength_scale = 1.0),
+    (grid_size = (128, 128, 32), enable_io = true, runlength_scale = 1.0),
+    (grid_size = (128, 128, 64), enable_io = true, runlength_scale = 1.0),
+    (grid_size = (256, 128, 64), enable_io = true, runlength_scale = 1.0),
+    (grid_size = (256, 256, 64), enable_io = true, runlength_scale = 1.0),
+    (grid_size = (256, 256, 128), enable_io = true, runlength_scale = 1.0),
+]
+
 df = DataFrame()
 for case in cases_grid
     run_data = run_benchmark_case(backend; case...)
     push!(df, repackge_run_data(run_data, case))
     # Override each timestep to preserve the data in case of faliure !
     CSV.write("scaling_run_gpu_withio.csv", df)
+end
+
+for case in cases_PISCES_grid
+    run_data = run_PISCES_benchmark_case(backend; case...)
+    push!(df, repackge_PISCES_run_data(run_data, case))
+    # Override each timestep to preserve the data in case of faliure !
+    CSV.write("scaling_PISCES_run_gpu_withio.csv", df)
 end
 
