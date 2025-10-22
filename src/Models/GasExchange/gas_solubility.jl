e508262df6429872e500ed7ddcf28d482b034dda
Jago Strong-Wright <jagoosw@protonmail.com>
2025-10-22 11:40:15 +0100

    type stability

--- src/Models/GasExchange/gas_solubility.jl
+++ src/Models/GasExchange/gas_solubility.jl
@@ -58,4 +58,4 @@ struct MolPerKgPerAtmToMMolPerCubicMPerMicroAtm{SO, DE}
        density :: DE
 end
 
-@inline (dss::MolPerKgPerAtmToMMolPerCubicMPerMicroAtm)(T, S) = dss.solubility(T + 273.15, S) * dss.density(T, S) / 10^3
+@inline (dss::MolPerKgPerAtmToMMolPerCubicMPerMicroAtm)(T::FT, S) where FT = dss.solubility(T + FT(273.15), S) * dss.density(T, S) / FT(10^3)
