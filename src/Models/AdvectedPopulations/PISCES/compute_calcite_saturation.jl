6078b72ec0735754d10f5e05056782ad938fe9a0
Jago Strong-Wright <jagoosw@protonmail.com>
2025-12-05 11:59:16 +0000

    maybe a more correct way todo it

--- src/Models/AdvectedPopulations/PISCES/compute_calcite_saturation.jl
+++ src/Models/AdvectedPopulations/PISCES/compute_calcite_saturation.jl
@@ -6,21 +6,19 @@ using Oceananigans.Utils: launch!
 
 using OceanBioME.Models: CarbonChemistryModel
 
-const gravitational_acceleration = defaults.gravitational_acceleration
-
 function compute_calcite_saturation!(carbon_chemistry, calcite_saturation, model)
     grid = model.grid
 
     arch = architecture(grid)
 
-    launch!(arch, grid, :xyz, _compute_calcite_saturation!, carbon_chemistry, calcite_saturation, grid, fields(model))
+    launch!(arch, grid, :xyz, _compute_calcite_saturation!, carbon_chemistry, calcite_saturation, grid, fields(model), defaults.gravitational_acceleration)
 
     fill_halo_regions!(calcite_saturation, model.grid, model.clock, fields(model))
 
     return nothing
 end
 
-@kernel function _compute_calcite_saturation!(carbon_chemistry, calcite_saturation, grid, model_fields)
+@kernel function _compute_calcite_saturation!(carbon_chemistry, calcite_saturation, grid, model_fields, g)
     i, j, k = @index(Global, NTuple)
 
     T = @inbounds model_fields.T[i, j, k]
@@ -33,7 +31,7 @@ end
 
     # very rough - don't think we should bother integrating the actual density
     # this is a type stability issue
-    P = abs(z) * gravitational_acceleration * 1026 / 100000
+    P = abs(z) * g * 1026 / 100000
 
     @inbounds calcite_saturation[i, j, k] = CarbonChemistryModel.calcite_saturation(carbon_chemistry; DIC, T, S, Alk, P, silicate)
 end
