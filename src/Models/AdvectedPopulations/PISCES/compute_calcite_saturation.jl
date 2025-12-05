aa311eddd840d516e378c42975b1d010957682b8
Jago Strong-Wright <jagoosw@protonmail.com>
2025-12-05 11:56:20 +0000

    Seems to fix the problem, but might be the wrong use of

--- src/Models/AdvectedPopulations/PISCES/compute_calcite_saturation.jl
+++ src/Models/AdvectedPopulations/PISCES/compute_calcite_saturation.jl
@@ -1,9 +1,12 @@
+using Oceananigans: defaults
 using Oceananigans.Architectures: architecture
 using Oceananigans.BoundaryConditions: fill_halo_regions!
 using Oceananigans.Models: fields
 using Oceananigans.Utils: launch!
 
 using OceanBioME.Models: CarbonChemistryModel
+
+const gravitational_acceleration = defaults.gravitational_acceleration
 
 function compute_calcite_saturation!(carbon_chemistry, calcite_saturation, model)
     grid = model.grid
@@ -12,7 +15,7 @@ function compute_calcite_saturation!(carbon_chemistry, calcite_saturation, model
 
     launch!(arch, grid, :xyz, _compute_calcite_saturation!, carbon_chemistry, calcite_saturation, grid, fields(model))
 
-    fill_halo_regions!(calcite_saturation)
+    fill_halo_regions!(calcite_saturation, model.grid, model.clock, fields(model))
 
     return nothing
 end
@@ -24,12 +27,13 @@ end
     S = @inbounds model_fields.S[i, j, k]
     DIC = @inbounds model_fields.DIC[i, j, k]
     Alk = @inbounds model_fields.Alk[i, j, k]
-    silicate = @inbounds model_fields.Si[i, j, k] # might get rid of this since it doesn't do anything
+    silicate = @inbounds model_fields.Si[i, j, k]
 
     z = znode(i, j, k, grid, Center(), Center(), Center())
 
     # very rough - don't think we should bother integrating the actual density
-    P = abs(z) * Oceananigans.defaults.gravitational_acceleration * 1026 / 100000
+    # this is a type stability issue
+    P = abs(z) * gravitational_acceleration * 1026 / 100000
 
     @inbounds calcite_saturation[i, j, k] = CarbonChemistryModel.calcite_saturation(carbon_chemistry; DIC, T, S, Alk, P, silicate)
 end
