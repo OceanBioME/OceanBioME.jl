49cb9d86497b969d65d381c0db38210c2142d7ad
nanophyto <coralscaper@gmail.com>
2026-03-09 23:09:50 +0000

    Refactor simple iron tracer tendency into a thin adapter

--- src/Models/AdvectedPopulations/PISCES/iron/simple_iron.jl
+++ src/Models/AdvectedPopulations/PISCES/iron/simple_iron.jl
@@ -17,47 +17,37 @@ required_biogeochemical_tracers(::SimpleIron) = tuple(:Fe)
 const SimpleIronPISCES = PISCES{<:Any, <:Any, <:Any, <:Any, <:Any, <:SimpleIron}
 
 @inline function (bgc::SimpleIronPISCES)(i, j, k, grid, val_name::Val{:Fe}, clock, fields, auxiliary_fields)
-    λ̄ = bgc.iron.excess_scavenging_enhancement
-
     Fe = @inbounds fields.Fe[i, j, k]
+    DOC = @inbounds fields.DOC[i, j, k]
 
-    λFe = iron_scavenging_rate(bgc.particulate_organic_matter, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
-    
-    Fe′ = free_iron(bgc.iron, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
+    T = @inbounds fields.T[i, j, k]
 
-    total_ligand_concentration = ligand_concentration(bgc.iron, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
+    scavenging_rate = iron_scavenging_rate(bgc.particulate_organic_matter, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
+    free_iron_concentration = free_iron(bgc.iron, Fe, DOC, T)
+    total_ligand_concentration = ligand_concentration(bgc.iron, DOC)
 
-    # terminal process which removes iron from the ocean
-    ligand_aggregation = λ̄ * λFe * max(0, Fe - total_ligand_concentration) * Fe′
+    ligand_aggregation_loss = ligand_aggregation(bgc.iron,
+                                                 scavenging_rate,
+                                                 Fe,
+                                                 total_ligand_concentration,
+                                                 free_iron_concentration)
 
     colloidal_aggregation, = aggregation_of_colloidal_iron(bgc.dissolved_organic_matter, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
 
-    # scavenging and bacterial uptake
     scavenging = iron_scavenging(bgc.particulate_organic_matter, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
+    bacterial_uptake = bacterial_iron_uptake(bgc.particulate_organic_matter, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
 
-    BactFe = bacterial_iron_uptake(bgc.particulate_organic_matter, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
-
-    # particle breakdown
     small_particles = degredation(bgc.particulate_organic_matter, Val(:SFe), i, j, k, grid, bgc, clock, fields, auxiliary_fields)
-
-    # consumption
     consumption = uptake(bgc.phytoplankton, val_name, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
-
-    # waste
     grazing_waste = non_assimilated_iron(bgc.zooplankton, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
-
     upper_trophic_waste = upper_trophic_dissolved_iron(bgc.zooplankton, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
 
-    return (small_particles + grazing_waste + upper_trophic_waste
-            - consumption - ligand_aggregation - colloidal_aggregation - scavenging - BactFe)
-end
-
-@inline function ligand_concentration(iron::SimpleIron, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
-    Lₜᵐᵃˣ = iron.maximum_ligand_concentration
-
-    DOC = @inbounds fields.DOC[i, j, k]
-
-    Lₜ = iron.dissolved_ligand_ratio * DOC - Lₜᵐᵃˣ
-    
-    return max(Lₜᵐᵃˣ, Lₜ)
+    return iron_tendency(small_particles,
+                         grazing_waste,
+                         upper_trophic_waste,
+                         consumption,
+                         ligand_aggregation_loss,
+                         colloidal_aggregation,
+                         scavenging,
+                         bacterial_uptake)
 end
