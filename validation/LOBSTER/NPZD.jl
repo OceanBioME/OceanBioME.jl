caccd439e2361b67ea4f9981176b418200c3519b
Jago Strong-Wright <jagoosw@protonmail.com>
2026-04-22 16:14:14 +0100

    more validation

--- validation/LOBSTER/NPZD.jl
+++ validation/LOBSTER/NPZD.jl
@@ -3,6 +3,15 @@ using OceanBioME.Models.LOBSTERModel: Nutrient, Detritus
 
 grid = RectilinearGrid(size = (1, 1, 10), extent = (1, 1, 10))
 
-biogeochemistry = LOBSTER(; grid, nutrients = Nutrient(), detritus = Detritus(), carbonate_system = CarbonateSystem())
+biogeochemistry = LOBSTER(; grid, biology = OceanBioME.Models.LOBSTERModel.PhytoZoo(temperature_coefficient = 1.88), nutrients = Nutrient(), detritus = Detritus(), carbonate_system = CarbonateSystem())
 
-model = NonhydrostaticModel(; grid, biogeochemistry)
+model = NonhydrostaticModel(grid; biogeochemistry, tracers = :T)
+
+time_step!(model, 1)
+
+# NPZ
+biogeochemistry = LOBSTER(; grid, nutrients = Nutrient(), detritus = nothing, carbonate_system = CarbonateSystem())
+
+model = NonhydrostaticModel(grid; biogeochemistry)
+
+time_step!(model, 1)
