0c29f87709b5bc4b4a63cd6bfb244bd5a6d57f8b
Jago Strong-Wright <jagoosw@protonmail.com>
2026-07-08 12:41:17 +0100

    please work

--- src/Models/AdvectedPopulations/NutrientsPlanktonDetritus/NutrientsPlanktonDetritus.jl
+++ src/Models/AdvectedPopulations/NutrientsPlanktonDetritus/NutrientsPlanktonDetritus.jl
@@ -16,9 +16,11 @@ using Oceananigans.Grids: AbstractGrid
 import Adapt: adapt_structure
 import Base: show, summary
 
-import Oceananigans.Biogeochemistry: 
+import Oceananigans.Biogeochemistry:
     required_biogeochemical_tracers,
     required_biogeochemical_auxiliary_fields,
+    biogeochemical_auxiliary_fields,
+    update_biogeochemical_state!,
     biogeochemical_drift_velocity
 
 # Consider all biogeochemical models to abstract to "Nutrients" (inorganics which can limit)
