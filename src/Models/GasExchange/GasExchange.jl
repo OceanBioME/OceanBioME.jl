bd261c1815f6ca2494854e07a0325ceae3dcdaa2
Jago Strong-Wright <jagoosw@protonmail.com>
2026-08-17 13:48:19 +0100

    fix some stuff

--- src/Models/GasExchange/GasExchange.jl
+++ src/Models/GasExchange/GasExchange.jl
@@ -114,11 +114,10 @@ function CarbonDioxideGasExchangeBoundaryCondition(FT = Float64;
                                                    air_concentration = 413, # ppmv
                                                    wind_speed = 2,
                                                    water_concentration = nothing,
-                                                   silicate_and_phosphate_names = nothing,
                                                    kwargs...)
 
     if isnothing(water_concentration)
-        water_concentration = CarbonDioxideConcentration(FT; carbon_chemistry, silicate_and_phosphate_names)
+        water_concentration = CarbonDioxideConcentration(FT; carbon_chemistry)
     elseif !isnothing(carbon_chemistry)
         @warn "Make sure that the `carbon_chemistry` $(carbon_chemistry) is the same as that in `water_concentration` $(water_concentration) (or set it to `nothing`)"
     end
