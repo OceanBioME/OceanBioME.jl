7b381e5b069a8af1cf24d2213d76f1133e6169e8
Jago Strong-Wright <jagoosw@protonmail.com>
2026-09-10 00:15:39 +0100

    stuff

--- src/Sediments/update_state.jl
+++ src/Sediments/update_state.jl
@@ -4,13 +4,15 @@ using Oceananigans.TimeSteppers: time_step!
 import Oceananigans.TimeSteppers: update_state!
 
 function update_biogeochemical_state!(model, sediment_model::BiogeochemicalSediment)
-    Δt = model.clock.last_stage_Δt
-
     update_tracked_fields!(sediment_model, model)
 
-    if isfinite(Δt)
-        time_step!(sediment_model, Δt;)
+    priming_call = (model.clock.iteration == 0) & (model.clock.stage == 1)
+
+    if !priming_call
+        step_sediment!(sediment_model, model, model.timestepper)
     end
+
+    update_state!(sediment_model)
 
     return nothing
 end
